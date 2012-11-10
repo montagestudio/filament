8a9db5e1308eeb43b7c6b177d499d14de7a3dc5a
simurai <simulus@gmail.com>
2012-11-10 10:56:51 -0800

    Added montage as submodule

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -1,20 +1,5 @@
 var Montage = require("montage/core/core").Montage,
-    Component = require("montage/ui/component").Component,
-    Connection = require("q-comm");
-
-if (typeof globals != "undefined") {
-    var backend = Connection(new WebSocket("ws://localhost:" + globals.nodePort));
-
-    // so you can play on the console:
-    global.backend = backend;
-
-    // DEMO
-    backend.get("fs").invoke("list", "/")
-    .then(function (list) {
-        console.log("File list from Node backend:", list);
-    })
-    .end();
-}
+    Component = require("montage/ui/component").Component;
 
 exports.Main = Montage.create(Component, {
 
