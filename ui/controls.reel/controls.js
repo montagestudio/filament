64cb4db7be076fe0a94b8aa138c48dfd084872f4
simurai <simulus@gmail.com>
2012-11-14 15:29:45 -0800

    Added controller

--- ui/controls.reel/controls.js
+++ ui/controls.reel/controls.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Header = Montage.create(Component, {});
+exports.Controls = Montage.create(Component, {});
