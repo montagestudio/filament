99e718c7767de059a48faf6dd6d550c64708cc70
Stuart Knightley <stuart@declarativ.com>
2012-11-19 10:14:47 -0800

    Add FileDialog to File inspector
    Allows images to be inserted. However they use Lumieres URLs
    and so will not work outside of Lumiere.

--- ui/inspector/file.reel/file.js
+++ ui/inspector/file.reel/file.js
@@ -18,6 +18,74 @@ exports.File = Montage.create(Component, /** @lends module:"ui/file.reel".File# 
 
     value: {
         value: null
+    },
+
+    // FIXME: Move into its own Filament module
+    _options: {
+        value: {
+            // whether to display as a roll down sheet
+            displayAsSheet: false,
+            // whether you can select files (otherwise just directories)
+            canChooseFiles: true,
+            // whether you can select directories (otherwise just files)
+            canChooseDirectories: false,
+            // can multiple files be selected
+            allowMultipleSelection: false,
+            // path of the directory to open on
+            defaultDirectory: null,
+            // array of file extensions/Uniform Type Identifiers
+            fileTypes: null
+        }
+    },
+
+    /**
+     * Opens the open dialog for the user
+     * @return {Promise<Array<string>>} A promise that is resolved with an
+     * array of filenames.
+     */
+    open: {
+        value: function() {
+            var deferred = Promise.defer();
+
+            // FIXME: Move into its own Filament module
+            FileDialog.openFile(this._options, function(success, files) {
+                console.log(success, files);
+                if (!success) {
+                    deferred.reject();
+                    return;
+                }
+
+                if (!Array.isArray(files)) {
+                    files = [files];
+                }
+
+                // We get "file://localhost" back from the FileDialog, change
+                // to "fs:/"
+                // TODO: copy file into project
+                for (var i = 0, len = files.length; i < len; i++) {
+                    files[i] = files[i].replace(/^file:\/\/localhost/, "fs:/");
+                }
+
+                deferred.resolve(files);
+            });
+
+            return deferred.promise;
+        }
+    },
+
+    prepareForActivationEvents: {
+        value: function() {
+            this.templateObjects.choose.addEventListener("action", this, false);
+        }
+    },
+
+    handleAction: {
+        value: function(event) {
+            var self = this;
+            this.open().then(function(files) {
+                self.value = files[0];
+            });
+        }
     }
 });
 
