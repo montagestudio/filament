eb529788618c0f0feee520fd05adf9fe3a3d49aa
simurai <simulus@gmail.com>
2012-12-10 21:07:53 +0100

    Added templates picker

--- ui/projects/history.reel/history.js
+++ ui/projects/history.reel/history.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.List = Montage.create(Component, {});
+exports.History = Montage.create(Component, {});
