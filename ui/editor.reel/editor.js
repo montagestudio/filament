dc39748b64cc65d3aee37eda5acaaef93e0ba448
simurai <simulus@gmail.com>
2012-12-12 02:29:53 +0100

    Added firstrun

--- ui/editor.reel/editor.js
+++ ui/editor.reel/editor.js
@@ -1,18 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Editor = Montage.create(Component, {
-
-    templateDidLoad: {
-        value: function() {
-            //console.log("main templateDidLoad")
-        }
-    },
-
-    prepareForDraw: {
-        value: function() {
-            //console.log("main prepareForDraw")
-        }
-    }
-
-});
+exports.Editor = Montage.create(Component, {});
--- ui/firstrun/firstrun.reel/firstrun.js
+++ ui/firstrun/firstrun.reel/firstrun.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Projects = Montage.create(Component, {});
+exports.Firstrun = Montage.create(Component, {});
