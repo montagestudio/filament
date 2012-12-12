9e0d0cc8f33692d92e355f5c38b06412b4ed714b
Jean-Francois Ducarroz <github@ducarroz.org>
2012-12-12 11:36:52 -0800

    Added the preferences app. Launched by Lumieres when the user select the menu Preferences

---
+++ preferences/ui/main.reel/main.js
@@ -0,0 +1,64 @@
+var Montage = require("montage/core/core").Montage,
+    Component = require("montage/ui/component").Component,
+    Connection = require("q-connection");
+
+var IS_IN_LUMIERES = (typeof lumieres !== "undefined");
+
+exports.Main = Montage.create(Component, {
+
+    _inputTimer: {
+        value: null
+    },
+
+    userPreferences: {
+        enumerable: true,
+        value: {}
+    },
+
+    didCreate: {
+        value: function() {
+            if (lumieres !== undefined && lumieres.getUserPreferences) {
+                var thisRef = this;
+                lumieres.getUserPreferences(function(success, result) {
+                    if (success) {
+                        thisRef.userPreferences = result;
+                    }
+                });
+            }
+        }
+    },
+
+    willDraw: {
+        value: function() {
+            this.httpportTextField.element.addEventListener("input", this);
+            this.httpportTextField.element.addEventListener("change", this);
+        }
+    },
+
+    handleInput: {
+        value: function(event) {
+            var thisRef = this;
+
+            if (this._inputTimer) {
+                clearTimeout(this._inputTimer);
+            }
+
+            this._inputTimer = setTimeout(function(){
+                thisRef._inputTimer = null;
+                thisRef.handleChange(event);
+            }, 1000);
+        }
+    },
+
+    handleChange: {
+        value: function(event) {
+            if (this._inputTimer) {
+                clearTimeout(this._inputTimer);
+                this._inputTimer = null;
+            }
+            // Save the preferences
+            lumieres.setUserPreferences(this.userPreferences);
+        }
+    }
+
+});
