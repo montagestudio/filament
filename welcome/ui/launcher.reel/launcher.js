eec88a5433f5f2502f77e32cdd11bbb1f93d6530
simurai <simulus@gmail.com>
2012-12-18 00:40:27 +0100

    Welcome screen is now stand-alone

--- welcome/ui/launcher.reel/launcher.js
+++ welcome/ui/launcher.reel/launcher.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Firstrun = Montage.create(Component, {});
+exports.Launcher = Montage.create(Component, {});
--- welcome/ui/main.reel/main.js
+++ welcome/ui/main.reel/main.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Overview = Montage.create(Component, {});
+exports.Main = Montage.create(Component, {});
