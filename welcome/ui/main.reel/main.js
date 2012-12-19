10ef7364f5c166c69bd9a3ef46b1a1e3bcf6448f
Mike Czepiel <mike@declarativ.com>
2012-12-19 14:01:19 -0800

    Combine welcome reels into a single reel
    It's not that complex an application. This also wires up some knowledge
    about being inside Lumieres; the version is now correctly displayed.

--- welcome/ui/main.reel/main.js
+++ welcome/ui/main.reel/main.js
@@ -1,4 +1,34 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Main = Montage.create(Component, {});
+var IS_IN_LUMIERES = (typeof lumieres !== "undefined");
+
+exports.Main = Montage.create(Component, {
+
+    version: {
+        value: "X"
+    },
+
+    didCreate: {
+        value: function () {
+
+            var self = this;
+            if (IS_IN_LUMIERES) {
+                this.version = lumieres.version;
+                require.async("core/lumieres-bridge").then(function (exported) {
+                    self.environmentBridge = exported.LumiereBridge.create();
+                });
+            } else {
+                require.async("core/browser-bridge").then(function (exported) {
+                    self.environmentBridge = exported.BrowserBridge.create();
+                });
+            }
+        }
+    },
+
+    //TODO not show ui until we have an environment bridge
+    //This would be a good case of the whole "custom loading scenario" idea
+    environmentBridge: {
+        value: null
+    }
+});
