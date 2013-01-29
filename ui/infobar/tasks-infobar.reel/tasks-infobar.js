579d58fe2b1a0c05fca38cebc4a2ed3c5d44ad35
Stuart Knightley <stuart@declarativ.com>
2013-01-29 15:31:56 -0800

    Add ugly but functioning infobar

--- ui/infobar/tasks-infobar.reel/tasks-infobar.js
+++ ui/infobar/tasks-infobar.reel/tasks-infobar.js
@@ -4,7 +4,8 @@
     @requires montage/ui/component
 */
 var Montage = require("montage").Montage,
-    Component = require("montage/ui/component").Component;
+    Component = require("montage/ui/component").Component,
+    Set = require("collections/set");
 
 /**
     Description TODO
@@ -12,5 +13,78 @@ var Montage = require("montage").Montage,
     @extends module:montage/ui/component.Component
 */
 exports.TasksInfobar = Montage.create(Component, /** @lends module:"ui/tasks-infobar.reel".TasksInfobar# */ {
+
+    didCreate: {
+        value: function() {
+            this._runningTasks = Set();
+            this._completedTasks = Set();
+            this._failedTasks = Set();
+        }
+    },
+
+    _runningTasks: {
+        value: null
+    },
+
+    _completedTasks: {
+        value: null
+    },
+
+    _failedTasks: {
+        value: null
+    },
+
+    addTask: {
+        value: function(promise, title, info) {
+            var self = this;
+
+            // TODO check if promise already exists
+
+            var task = {
+                promise: promise,
+                title: title || "Unnamed task",
+                info: info || "",
+            };
+
+            promise.then(function (value) {
+                task.info = value;
+                self._runningTasks.delete(task);
+                self._completedTasks.add(task);
+                self.needsDraw = true;
+            }, function (err) {
+                task.info = err;
+                self._runningTasks.delete(task);
+                self._failedTasks.add(task);
+                self.needsDraw = true;
+            }, function (info) {
+                task.info = info;
+                self.needsDraw = true;
+            });
+
+            this._runningTasks.add(task);
+            this.needsDraw = true;
+            this.infobar.show();
+        }
+    },
+
+    handleInfobarClosed: {
+        value: function (event) {
+            this._completedTasks.clear();
+            this._failedTasks.clear();
+            this.needsDraw = true;
+        }
+    },
+
+    draw: {
+        value: function() {
+            var out = "";
+
+            out += this._runningTasks.length + " running ";
+            out += this._completedTasks.length + " completed ";
+            out += this._failedTasks.length + " failed ";
+
+            this.templateObjects.title.value = out;
+        }
+    }
 
 });
