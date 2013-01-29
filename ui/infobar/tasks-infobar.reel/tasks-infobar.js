35f744efa5cdb7388e275be1047cc7841c2691f0
Stuart Knightley <stuart@declarativ.com>
2013-01-29 14:10:00 -0800

    Add tasks infobar

---
+++ ui/infobar/tasks-infobar.reel/tasks-infobar.js
@@ -0,0 +1,16 @@
+/**
+    @module "ui/tasks-infobar.reel"
+    @requires montage
+    @requires montage/ui/component
+*/
+var Montage = require("montage").Montage,
+    Component = require("montage/ui/component").Component;
+
+/**
+    Description TODO
+    @class module:"ui/tasks-infobar.reel".TasksInfobar
+    @extends module:montage/ui/component.Component
+*/
+exports.TasksInfobar = Montage.create(Component, /** @lends module:"ui/tasks-infobar.reel".TasksInfobar# */ {
+
+});
