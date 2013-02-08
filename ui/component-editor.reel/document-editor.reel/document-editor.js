7902b7707675aa3d8f4f6766120b7052bff1e227
Mike Czepiel <mike@declarativ.com>
2013-02-07 20:54:25 -0800

    Make componentEditor load workbench and editingDoc
    The workbench component stack is no longer responsible for loading the
    editing model i.e. the view no longer loads the model.

--- ui/component-editor.reel/document-editor.reel/document-editor.js
+++ ui/component-editor.reel/document-editor.reel/document-editor.js
@@ -1,7 +1,9 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component,
     Deserializer = require("montage/core/deserializer").Deserializer,
-    MimeTypes = require("core/mime-types");
+    ReelDocument = require("palette/core/reel-document").ReelDocument,
+    MimeTypes = require("core/mime-types"),
+    Promise = require("montage/core/promise").Promise;
 
 exports.DocumentEditor = Montage.create(Component, {
 
@@ -23,31 +25,50 @@ exports.DocumentEditor = Montage.create(Component, {
         value: null
     },
 
-    load: {
-        value: function (fileUrl, packageUrl) {
-            var self = this,
-                blueprintPromise,
-                stageObject;
-
-            Object.defineBinding(self, "fileUrl", {
-                boundObject: self,
+    didCreate: {
+        value: function () {
+            Object.defineBinding(this, "fileUrl", {
+                boundObject: this,
                 boundObjectPropertyPath: "editingDocument.fileUrl",
                 oneway: true
             });
+        }
+    },
 
-            return this.workbench.load(fileUrl, packageUrl).then(function (editingDocument) {
-                self.editingDocument = editingDocument;
+    load: {
+        value: function (fileUrl, packageUrl) {
+            var self = this,
+                liveStageInfoPromise = this.workbench.load(fileUrl, packageUrl),
+                editingDocumentPromise,
+                moduleId,
+                exportName;
 
-                editingDocument.editingProxies.forEach(function (proxy) {
-                    stageObject = proxy.stageObject;
-                    blueprintPromise = stageObject.blueprint;
-                    if (blueprintPromise) {
-                        blueprintPromise.fail(Function.noop);
-                    }
+
+            editingDocumentPromise = require.loadPackage(packageUrl)
+                .then(function (packageRequire) {
+                    return ReelDocument.load(fileUrl, packageRequire)
+                        .then(function (reelDocument) {
+
+                            // pre-load blueprints for everything already on the stage
+                            // but don't complain if we can't find one
+                            reelDocument.editingProxies.forEach(function (proxy) {
+                                moduleId = proxy.moduleId;
+                                exportName = proxy.exportName;
+                                if (moduleId && exportName) {
+                                    packageRequire.async(moduleId).get(exportName).get("blueprint").fail(Function.noop);
+                                }
+                            });
+
+                            return reelDocument;
+                        });
                 });
 
+            //Wait for the stage and the editingDocument: only return the editingDocument
+            return Promise.all([liveStageInfoPromise, editingDocumentPromise]).spread(function (liveStageInfo, editingDocument) {
+                editingDocument.associateWithLiveRepresentations(liveStageInfo.owner, liveStageInfo.template, liveStageInfo.frame);
+                self.editingDocument = editingDocument;
                 return editingDocument;
-            });
+            }).timeout(10000);
         }
     },
 
@@ -55,6 +76,7 @@ exports.DocumentEditor = Montage.create(Component, {
         value: function () {
             this.workbench.addEventListener("dragover", this, false);
             this.workbench.addEventListener("drop", this, false);
+            this.workbench.addEventListener("select", this, false);
         }
     },
 
@@ -103,6 +125,34 @@ exports.DocumentEditor = Montage.create(Component, {
                     prototypeEntry.postProcess(proxy, editingDocument);
                 }
             }).done();
+        }
+    },
+
+    handleSelect: {
+        value: function (evt) {
+            var detail = evt.detail,
+                selectionCandidate = detail.candidate.controller,
+                isAddingToSelection = detail.addingToSelection,
+                isRemovingFromSelection = detail.removingFromSelection,
+                editingDocument = this.editingDocument;
+
+            if (selectionCandidate) {
+
+                selectionCandidate = editingDocument.editingProxyForObject(selectionCandidate);
+
+                //TODO make the element associated with the current selection invisible to point events to drill through a tree
+
+                if (isAddingToSelection) {
+                    editingDocument.selectObject(selectionCandidate);
+                } else if (isRemovingFromSelection) {
+                    editingDocument.deselectObject(selectionCandidate);
+                } else {
+                    editingDocument.clearSelectedObjects();
+                    editingDocument.selectObject(selectionCandidate);
+                }
+            } else {
+                editingDocument.clearSelectedObjects();
+            }
         }
     },
 
