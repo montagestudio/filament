19ee181d12ffa8a5e0722c030e46c0cf73c46baf
simurai <simulus@gmail.com>
2013-02-12 16:04:35 +0100

    Removed wireframe code in nav

--- ui/nav.reel/nav.js
+++ ui/nav.reel/nav.js
@@ -1,31 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Nav = Montage.create(Component, {
-
-    prepareForDraw: {
-        value: function() {
-            /*
-            var main = document.getElementById('main');
-            var device = document.getElementById('device');
-            var libraryButton = document.getElementById('libraryButton');
-            var explorerButton = document.getElementById('explorerButton');
-            
-            libraryButton.onclick = function() {
-                explorerButton.classList.remove('active');
-                this.classList.add('active');
-                main.classList.remove('isExplorer');
-                main.classList.add('isLibrary');
-            }
-            
-            explorerButton.onclick = function() {
-                libraryButton.classList.remove('active');
-                this.classList.add('active');
-                main.classList.remove('isLibrary');
-                main.classList.add('isExplorer');
-            }
-            */
-        }
-    }
-
-});
+exports.Nav = Montage.create(Component, {});
