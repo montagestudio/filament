06ab57a66feca13ec39c67f6d61d7a9b5b268cb9
Pierre Frisch <pierre.frisch@spearway.com>
2013-03-01 09:06:53 -0800

    Created a super class for the editor

--- extensions/blueprint-editor.filament-extension/ui/blueprint-editor.reel/blueprint-editor.js
+++ extensions/blueprint-editor.filament-extension/ui/blueprint-editor.reel/blueprint-editor.js
@@ -1,110 +1,28 @@
 /**
  @module "./editor.reel"
  @requires montage
- @requires montage/ui/component
+ @requires palette/ui/editor.reel
  */
 var Montage = require("montage").Montage;
-var Component = require("montage/ui/component").Component;
 var BlueprintDocument = require("core/blueprint-document").BlueprintDocument;
-var Promise = require("montage/core/promise").Promise;
-var Map = require("montage/collections/map");
+var Editor = require("palette/ui/editor.reel").Editor;
 
 /**
  Description TODO
  @class module:"./Editor.reel".Editor
- @extends module:montage/ui/component.Component
+ @extends module:palette/ui/editor.reel.Editor
  */
-exports.BlueprintEditor = Montage.create(Component, /** @lends module:"./viewer.reel".Viewer# */ {
+exports.BlueprintEditor = Montage.create(Editor, /** @lends module:"./viewer.reel".Viewer# */ {
 
-    _currentDocument:{
-        value:null
-    },
-
-    currentDocument:{
-        get:function () {
-            return this._currentDocument;
-        }
-    },
-
-    _fileUrlDocumentMap:{
-        value:null
-    },
-
-    didCreate:{
-        value:function () {
-            this._fileUrlDocumentMap = new Map();
-        }
-    },
-
-    load:{
+    /*
+     * Create and load the document.<br/>
+     * <b>Note:</> This must be overwritten by sub classes
+     * @returns By default a rejected promise.
+     */
+    loadDocument:{
         value:function (fileUrl, packageUrl) {
-            var self = this;
-            var documentPromise = this._fileUrlDocumentMap.get(fileUrl);
-            if (documentPromise) {
-                // the document was already open
-                return documentPromise.then(function (document) {
-                    self.dispatchBeforeOwnPropertyChange("currentDocument", self._currentDocument);
-                    self._currentDocument = document;
-                    self.dispatchOwnPropertyChange("currentDocument", document);
-                });
-            }
-            documentPromise = BlueprintDocument.load(fileUrl, packageUrl);
-            this._fileUrlDocumentMap.set(fileUrl, documentPromise);
-            return documentPromise.then(function (document) {
-                self.dispatchBeforeOwnPropertyChange("currentDocument", self._currentDocument);
-                self._currentDocument = document;
-                self.dispatchOwnPropertyChange("currentDocument", document);
-                return document;
-            }, function (error) {
-                self._fileUrlDocumentMap.delete(fileUrl);
-                console.log("Editor could not load document " + fileUrl, error);
-                return Promise.reject(new Error("Editor could not load document " + fileUrl));
-            });
+            return BlueprintDocument.load(fileUrl, packageUrl);
         }
-    },
-
-
-    close:{
-        value:function (fileUrl) {
-            var self = this;
-            var documentPromise = this._fileUrlDocumentMap.get(fileUrl);
-
-            if (documentPromise) {
-                this._fileUrlDocumentMap.delete(fileUrl);
-
-                return documentPromise.then(function (document) {
-                    if (document == self._currentDocument) {
-
-                        var urls = self._fileUrlDocumentMap.keys;
-                        var newCurrentDocumentPromise = null;
-                        if (urls && (urls.length > 0)) {
-                            newCurrentDocumentPromise = self._fileUrlDocumentMap.get(urls[0]);
-                        }
-                        if (newCurrentDocumentPromise) {
-                            newCurrentDocumentPromise.then(function (newCurrentDocument) {
-                                self.dispatchBeforeOwnPropertyChange("currentDocument", self._currentDocument);
-                                self._currentDocument = newCurrentDocument;
-                                self.dispatchOwnPropertyChange("currentDocument", newCurrentDocument);
-                                self.needsDraw = true;
-                                return documentPromise;
-                            });
-                        } else {
-                            self.dispatchBeforeOwnPropertyChange("currentDocument", self._currentDocument);
-                            self._currentDocument = null;
-                            self.dispatchOwnPropertyChange("currentDocument", null);
-                            self.needsDraw = true;
-                            return documentPromise;
-                        }
-                    } else {
-                        return documentPromise;
-                    }
-                });
-            } else {
-                console.log("We don't have an open document for " + fileUrl);
-            }
-            return Promise.resolve(null);
-        }
-    },
-
+    }
 
 });
--- extensions/image-viewer.filament-extension/ui/viewer.reel/viewer.js
+++ extensions/image-viewer.filament-extension/ui/viewer.reel/viewer.js
@@ -1,30 +1,28 @@
 /**
-    @module "./viewer.reel"
-    @requires montage
-    @requires montage/ui/component
-*/
+ @module "./viewer.reel"
+ @requires montage
+ @requires montage/ui/component
+ */
 var Montage = require("montage").Montage,
-    Component = require("montage/ui/component").Component,
     ImageDocument = require("core/image-document").ImageDocument,
-    Promise = require("montage/core/promise").Promise;
+    Promise = require("montage/core/promise").Promise,
+    Editor = require("palette/ui/editor.reel").Editor;
 
 /**
-    Description TODO
-    @class module:"./viewer.reel".Viewer
-    @extends module:montage/ui/component.Component
-*/
-exports.Viewer = Montage.create(Component, /** @lends module:"./viewer.reel".Viewer# */ {
+ Description TODO
+ @class module:"./viewer.reel".Viewer
+ @extends module:palette/ui/editor.reel.Editor
+ */
+exports.Viewer = Montage.create(Editor, /** @lends module:"./viewer.reel".Viewer# */ {
 
-    currentDocument: {
-        value: null
-    },
-
-    load: {
-        value: function (fileUrl, packageUrl) {
-            //TODO not make a new document each time..
-            var document = ImageDocument.create().init(fileUrl, packageUrl);
-            this.currentDocument = document;
-            return Promise.resolve(document);
+    /*
+     * Create and load the document.<br/>
+     * <b>Note:</> This must be overwritten by sub classes
+     * @returns By default a rejected promise.
+     */
+    loadDocument:{
+        value:function (fileUrl, packageUrl) {
+            return Promise.resolve(ImageDocument.create().init(fileUrl, packageUrl));
         }
     }
 
