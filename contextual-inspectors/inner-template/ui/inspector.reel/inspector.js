7c10f6c2c27eb024ea93c34734a2add8cc4a761c
Stuart Knightley <stuart@declarativ.com>
2013-03-05 11:53:10 -0800

    Add empty inner template contextual inspector

---
+++ contextual-inspectors/inner-template/ui/inspector.reel/inspector.js
@@ -0,0 +1,16 @@
+/**
+    @module "ui//inspector.reel"
+    @requires montage
+    @requires montage/ui/component
+*/
+var Montage = require("montage").Montage,
+    Component = require("montage/ui/component").Component;
+
+/**
+    Description TODO
+    @class module:"ui//inspector.reel".Inspector
+    @extends module:montage/ui/component.Component
+*/
+exports.Inspector = Montage.create(Component, /** @lends module:"ui//inspector.reel".Inspector# */ {
+
+});
