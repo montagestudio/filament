cd8e8aa4b2cc5debbbbbcc5a3e0d1b060970adf3
simurai <simulus@gmail.com>
2013-03-08 17:13:37 +0100

    Added Library mode toggles
    Just the middle "grid" one actually works

--- ui/library.reel/library.js
+++ ui/library.reel/library.js
@@ -1,4 +1,35 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Library = Montage.create(Component, {});
+exports.Library = Montage.create(Component, {
+    
+    prepareForDraw: {
+        value: function() {
+            
+            var currentDisplay = "Library--grid";
+            var LibraryGrid = document.getElementById('LibraryGrid');
+            var LibraryGridButton = document.getElementById('LibraryGridButton');
+            
+            LibraryGridButton.onclick = function() {
+                
+                if(currentDisplay == 'Library--grid') {
+                    LibraryGrid.classList.remove('Library--grid');
+                    LibraryGrid.classList.add('Library--withDetails');
+                    currentDisplay = 'Library--withDetails';
+                
+                } else if (currentDisplay == 'Library--withDetails') {
+                    LibraryGrid.classList.remove('Library--withDetails');
+                    LibraryGrid.classList.add('Library--labelOnly');
+                    currentDisplay = 'Library--labelOnly';
+                
+                } else if (currentDisplay == 'Library--labelOnly') {
+                    LibraryGrid.classList.remove('Library--labelOnly');
+                    LibraryGrid.classList.add('Library--grid');
+                    currentDisplay = 'Library--grid';
+                }
+            }
+                        
+        }
+    }
+
+});
--- ui/package-explorer.reel/package-explorer.js
+++ ui/package-explorer.reel/package-explorer.js
@@ -1,20 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.PackageExplorer = Montage.create(Component, {
-
-    packageDescription: {
-        value: null
-    },
-
-    files: {
-        value: null
-    },
-
-    handleAddFileButtonAction: {
-        value: function (evt) {
-            this.dispatchEventNamed("addFile", true, true);
-        }
-    }
-
-});
+exports.PackageExplorer = Montage.create(Component, {});
