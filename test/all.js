b897214f805c57511c7cde380fd18fcf925cc8ae
Mike Czepiel <mike@declarativ.com>
2013-03-20 14:47:17 -0700

    Remove extraneous tests
    Cleaning from my work on another branch.

--- test/all.js
+++ test/all.js
@@ -1,7 +1,5 @@
 require("montage-testing").run(require, [
     // Please keep in alphabetical order
     "test/core/project-controller-creation-spec",
-    "test/core/project-controller-extension-spec",
-    "test/core/project-controller-file-spec",
-    "test/core/project-controller-spec"
+    "test/core/project-controller-file-spec"
 ]);
