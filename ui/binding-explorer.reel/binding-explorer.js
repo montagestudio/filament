cd28cda6c50b0ca4d0973c46e8ed5d1b8d5e90a3
Stuart Knightley <stuart@declarativ.com>
2013-04-15 16:23:18 -0700

    Accept dragged template objects on binding explorer
    to start creating a binding

--- ui/binding-explorer.reel/binding-explorer.js
+++ ui/binding-explorer.reel/binding-explorer.js
@@ -19,12 +19,52 @@ exports.BindingExplorer = Montage.create(Component, /** @lends module:"./binding
         }
     },
 
+    enterDocument: {
+        value: function (firstTime) {
+            if (!firstTime) { return; }
+            // templateObjects.addButton
+            this._defineBindingElements = this.templateObjects.defineBindingButton.map(function (component) {
+                return component.element;
+            });
+
+            this._element.addEventListener("dragover", this, false);
+            this._element.addEventListener("drop", this, false);
+        }
+    },
+
     templateObjectsController: {
         value: null
     },
 
     editingDocument: {
         value: null
+    },
+
+    handleDragover: {
+        enumerable: false,
+        value: function (event) {
+            if (
+                this.templateObjects.defineBindingButton.indexOf(event.target.component) !== -1 &&
+                event.dataTransfer.types.indexOf("text/plain") !== -1
+            ) {
+                // allows us to drop
+                event.preventDefault();
+                event.dataTransfer.dropEffect = "copy";
+            } else {
+                event.dataTransfer.dropEffect = "none";
+            }
+        }
+    },
+    handleDrop: {
+        value: function (evt) {
+            var data = evt.dataTransfer.getData("text/plain");
+            if (data.substr(0, 1) !== "@") { return; }
+
+            var bindingModel = Object.create(null);
+            bindingModel.targetObject = evt.target.component.detail.get("targetObject");
+            bindingModel.sourcePath = data;
+            this.templateObjects.bindingCreator.bindingModel = bindingModel;
+        }
     },
 
     handleDefineBindingButtonAction: {
