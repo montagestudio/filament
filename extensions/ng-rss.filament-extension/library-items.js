71d2a52abbe3597d3b59d322de4ae44b3952c1e3
Stuart Knightley <stuart@declarativ.com>
2013-04-16 21:27:10 -0700

    DEMO: Add extension for ng-rss package

---
+++ extensions/ng-rss.filament-extension/library-items.js
@@ -0,0 +1,62 @@
+var Montage = require("montage").Montage;
+var LibraryItem = require("filament-extension/core/library-item.js").LibraryItem;
+
+//TODO assets like the icons for use in library items should be versioned with the extensions themselves
+//TODO each extension really should be its own package, anticipate consuming extensions from elsewhere
+var packageLocation = require.location;
+
+var RssControllerLibraryItem = Montage.create(LibraryItem, {
+
+    serialization: {
+        value: {
+            "prototype": "ng-rss/rss-controller"
+        }
+    },
+
+    name: {
+        value: "RSS Controller"
+    },
+
+    label: {
+        value: "RSS Controller"
+    },
+
+    icon: {
+        value: packageLocation + "assets/rss-controller.png"
+    }
+});
+
+var RssViewLibraryItem = Montage.create(LibraryItem, {
+
+    serialization: {
+        value: {
+            "prototype": "ng-rss/ui/rss-view.reel",
+            "properties": {
+                "element": {"#": "rssView"}
+            }
+        }
+    },
+
+    name: {
+        value: "RSS View"
+    },
+
+    label: {
+        value: "RSS View"
+    },
+
+    icon: {
+        value: packageLocation + "assets/rss-view.png"
+    },
+
+    html: {
+        value: '<div data-montage-id="rssView" class="rssDemo-Main-rssView"></div>'
+    }
+
+});
+
+exports.libraryItems = {
+    "ng-rss/ui/rss-view.reel": RssViewLibraryItem,
+};
+
+exports.libraryAdditions = [RssControllerLibraryItem];
