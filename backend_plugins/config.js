d573328fd8191b946045740ab43811bebd2851f9
Jean-Francois Ducarroz <github@ducarroz.org>
2013-05-09 11:59:35 -0700

    Hide the toolbar in the About window

--- backend_plugins/config.js
+++ backend_plugins/config.js
@@ -70,7 +70,7 @@ exports.rootURLForFileURL = function(object) {
 };
 
 exports.aboutPageInfo = function() {
-    return {url: "http://client/about/index.html", width:906, height:540};
+    return {url: "http://client/about/index.html", width:906, height:540, showToolbar: false};
 };
 
 exports.welcomePageInfo = function() {
