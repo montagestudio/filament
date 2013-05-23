a7b54233a810a6783c7061f6a76eea0c1a7e64c7
Fabrice Robinet <fabricerobinet@me.com>
2013-05-22 22:48:00 -0700

    drive the colladaView resizing from the editor

--- extensions/collada-editor.filament-extension/ui/collada-editor.reel/collada-editor.js
+++ extensions/collada-editor.filament-extension/ui/collada-editor.reel/collada-editor.js
@@ -1,4 +1,38 @@
 var Montage = require("montage").Montage,
     Editor = require("palette/ui/editor.reel").Editor;
 
-exports.ColladaEditor = Montage.create(Editor, {});
+exports.ColladaEditor = Montage.create(Editor, {
+
+    captureResize: {
+        value: function(evt) {
+            this.needsDraw = true;
+        }
+    },
+
+    willDraw: {
+        value: function() {
+            if (this.templateObjects) {
+                //FIXME:(FR) "manual" propagation the size of the editor slot to the colladaView.
+                //WebGL viewport needs this information.
+                if (this.templateObjects.colladaView) {
+                    this.templateObjects.colladaView.width = this.element.parentElement.offsetWidth;
+                    this.templateObjects.colladaView.height = this.element.parentElement.offsetHeight;
+                }
+            }
+        }
+    },
+
+    enterDocument: {
+        value: function() {
+            window.addEventListener("resize", this, true);
+        }
+    },
+
+    exitDocument: {
+        value: function() {
+            window.removeEventListener("resize", this, true);
+        }
+    }
+
+
+});
