d5bf383df8f98b4c069f50def9e9020c8830df1f
Stuart Knightley <stuart@declarativ.com>
2013-05-24 14:22:54 -0700

    Allow element to be missing when reviving

--- core/serialization/reel-reviver.js
+++ core/serialization/reel-reviver.js
@@ -1,4 +1,5 @@
 var Montage = require("montage").Montage,
+    Promise = require("montage/core/promise").Promise,
     ProxyReviver = require("palette/core/serialization/proxy-reviver").ProxyReviver,
     ReelProxy = require("core/reel-proxy").ReelProxy;
 
@@ -22,7 +23,27 @@ exports.ReelReviver = Montage.create(ProxyReviver, {
                 exportId = context.ownerExportId;
             }
 
-            return proxyObject.init(label, revivedSerialization, exportId, context.editingDocument);
+            if (Promise.isPromise(revivedSerialization)) {
+                return revivedSerialization.then(function (revivedSerialization) {
+                    return proxyObject.init(label, revivedSerialization, exportId, context.editingDocument);
+                });
+            } else {
+                return proxyObject.init(label, revivedSerialization, exportId, context.editingDocument);
+            }
+        }
+    },
+
+    // We want to be forgiving if an element can't be found, so that the
+    // user can add one later
+    reviveElement: {
+        value: function () {
+            var el = ProxyReviver.reviveElement.apply(this, arguments);
+
+            if (Promise.isPromise(el) && el.isRejected()) {
+                return void 0;
+            }
+
+            return el;
         }
     },
 
