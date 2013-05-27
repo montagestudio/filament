843ab3ed3fd5818217f8845188d7664a370ae097
Fabrice Robinet <fabricerobinet@me.com>
2013-05-27 13:17:18 -0700

    add collada components & library items

---
+++ extensions/collada.filament-extension/extension.js
@@ -0,0 +1,46 @@
+var Montage = require("montage/core/core").Montage,
+    CoreExtension = require("filament-extension/core/extension").Extension,
+    Promise = require("montage/core/promise").Promise,
+    libraryItems = require("library-items").libraryItems,
+    libraryAdditions = require("library-items").libraryAdditions;
+
+var Extension = exports.Extension = CoreExtension.specialize( {
+
+    constructor: {
+        value: function Extension() {
+            this.super();
+        }
+    },
+
+    activate: {
+        value: function (application, projectController) {
+
+            Object.keys(libraryItems).forEach(function (moduleId) {
+                projectController.registerLibraryItemForModuleId(libraryItems[moduleId], moduleId);
+            });
+
+            libraryAdditions.forEach(function (libraryItem) {
+                projectController.registerLibraryItemForPackageName(libraryItem, "glTF-webgl-viewer");
+            });
+
+            return Promise.resolve(this);
+        }
+    },
+
+    deactivate: {
+        value: function (application, projectController) {
+
+            Object.keys(libraryItems).forEach(function (moduleId) {
+                projectController.unregisterLibraryItemForModuleId(moduleId);
+            });
+
+
+            libraryAdditions.forEach(function (libraryItem) {
+                projectController.unregisterLibraryItemForPackageName(libraryItem, "glTF-webgl-viewer");
+            });
+
+            return Promise.resolve(this);
+        }
+    }
+
+});
---
+++ extensions/collada.filament-extension/library-items.js
@@ -0,0 +1,46 @@
+var Montage = require("montage").Montage;
+var LibraryItem = require("filament-extension/core/library-item").LibraryItem;
+
+//TODO assets like the icons for use in library items should be versioned with the extensions themselves
+//TODO each extension really should be its own package, anticipate consuming extensions from elsewhere
+var packageLocation = require.location;
+
+
+var ColladaViewLibraryItem = LibraryItem.specialize( {
+
+    serialization: {
+        value: {
+            "prototype": "glTF-webgl-viewer/ui/view.reel",
+            "properties": {
+                "element": {
+                    "#": "component"
+                }
+            }
+        }
+    },
+
+    name: {
+        value: "Collada View"
+    },
+
+    label: {
+        value: "Collada View"
+    },
+
+    icon: {
+        value: packageLocation + "assets/components/collada-view.png"
+    },
+
+    html: {
+        value: '<div data-montage-id="component"><canvas data-montage-id="canvas"></canvas></div> '
+    }
+
+});
+
+
+//TODO build this automatically
+exports.libraryItems = {
+    "glTF-webgl-viewer/ui/view.reel": ColladaViewLibraryItem,
+};
+
+exports.libraryAdditions = [];
