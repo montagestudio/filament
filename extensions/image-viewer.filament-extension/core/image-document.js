0c157ea172ab3738e15d0ca61da7686c82a40e20
Mike Czepiel <mike@declarativ.com>
2013-06-24 14:13:46 -0700

    Give image extensions document a constructor
    This should be the lats extension I really need to do this for.

--- extensions/image-viewer.filament-extension/core/image-document.js
+++ extensions/image-viewer.filament-extension/core/image-document.js
@@ -2,9 +2,16 @@ var Montage = require("montage").Montage,
     EditingDocument = require("palette/core/editing-document").EditingDocument,
     Viewer = require("ui/viewer.reel").Viewer;
 
-exports.ImageDocument = Montage.create(EditingDocument, {
+exports.ImageDocument = EditingDocument.specialize({
+
+    constructor: {
+        value: function ImageDocument () {
+            this.super();
+        }
+    }
 
 }, {
+
     editorType: {
         get: function () {
             return Viewer;
