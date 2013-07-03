8a8351f53495433d91fccec763d4196e88a179d8
Stuart Knightley <stuart@declarativ.com>
2013-07-03 13:48:25 -0700

    Add simple email mock data

---
+++ ui/template-explorer.reel/owner-cell.reel/mock-data.reel/mock-data.js
@@ -0,0 +1,16 @@
+/**
+    @module "ui/mock-data.reel"
+    @requires montage
+    @requires montage/ui/component
+*/
+var Montage = require("montage").Montage,
+    Component = require("montage/ui/component").Component;
+
+/**
+    Description TODO
+    @class module:"ui/mock-data.reel".MockData
+    @extends module:montage/ui/component.Component
+*/
+exports.MockData = Montage.create(Component, /** @lends module:"ui/mock-data.reel".MockData# */ {
+
+});
