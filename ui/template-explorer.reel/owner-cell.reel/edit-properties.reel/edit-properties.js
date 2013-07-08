1c83fbff5225c71663c4c7959f8b2b0b8457dbcb
Stuart Knightley <stuart@declarativ.com>
2013-07-08 12:28:24 -0700

    Get the blueprint in the owner properties editor

--- ui/template-explorer.reel/owner-cell.reel/edit-properties.reel/edit-properties.js
+++ ui/template-explorer.reel/owner-cell.reel/edit-properties.reel/edit-properties.js
@@ -13,4 +13,51 @@ var Montage = require("montage").Montage,
 */
 exports.EditProperties = Montage.create(Component, /** @lends module:"ui/edit-properties.reel".EditProperties# */ {
 
+    _ownerObject: {
+        value: null
+    },
+    ownerObject: {
+        get: function() {
+            return this._ownerObject;
+        },
+        set: function(value) {
+            if (this._ownerObject === value) {
+                return;
+            }
+            this._ownerObject = value;
+
+            if (value) {
+                var self = this;
+
+                value.packageRequire.async(value.moduleId)
+                .get(value.exportName)
+                .get("blueprint")
+                .then(function (blueprint) {
+                    self.ownerBlueprint = blueprint;
+                })
+                .done();
+            } else {
+                this.ownerBlueprint = void 0;
+            }
+        }
+    },
+
+    _ownerBlueprint: {
+        value: null
+    },
+    ownerBlueprint: {
+        get: function() {
+            return this._ownerBlueprint;
+        },
+        set: function(value) {
+            if (this._ownerBlueprint === value) {
+                return;
+            }
+            this._ownerBlueprint = value;
+            if (value) {
+                this.properties = value.propertyBlueprintGroupForName("default");
+            }
+        }
+    }
+
 });
