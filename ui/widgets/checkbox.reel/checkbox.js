5c820daeded35c004fe7c250f52265acdf956196
simurai <simulus@gmail.com>
2013-07-20 01:32:20 +0900

    Filament Checkbox styles

--- ui/widgets/checkbox.reel/checkbox.js
+++ ui/widgets/checkbox.reel/checkbox.js
@@ -1,9 +1,14 @@
 var AbstractCheckbox = require("montage/ui/base/abstract-checkbox").AbstractCheckbox;
 
 exports.Checkbox = AbstractCheckbox.specialize({
+    
+    hasTemplate: {value: true},
+    
     constructor: {
         value: function Checkbox() {
             this.super();
+            
+            this.classList.add("filament-Checkbox");
         }
     }
 });
