4615036512fb3cf45b53af4ce782663c2d474ec4
Stuart Knightley <stuart@declarativ.com>
2013-07-29 11:09:14 -0700

    Revert "Remove mop from backend plugins"
    This reverts commit 69d71ffccd78740429c409ca6ca016fe61fac065.

---
+++ backend_plugins/mop.js
@@ -0,0 +1,14 @@
+var Q = require("q");
+var optimize = require("mop");
+
+exports.optimize = function(location, config) {
+    var slice = Array.prototype.slice;
+
+    var status = config.out.status;
+
+    config.out.status = function() {
+        Q.fapply(status, slice.call(arguments));
+    };
+
+    return optimize(location, config);
+};
