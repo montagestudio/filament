91a7e7df3455b128e98a6cfd3aca716721cad0ca
Stuart Knightley <stuart@declarativ.com>
2013-08-01 12:04:32 -0700

    Split up owner blueprint editing spec
    into the UI spec, and the reel-document spec

---
+++ test/core/reel-blueprint-editing-spec.js
@@ -0,0 +1,96 @@
+var Promise = require("montage/core/promise").Promise;
+var Blueprint = require("montage/core/meta/blueprint").Blueprint;
+var PropertyBlueprint = require("montage/core/meta/property-blueprint").PropertyBlueprint;
+var Template = require("montage/core/template").Template;
+var mockReelDocument = require("test/mocks/reel-document-mocks").mockReelDocument;
+
+
+describe("core/reel-blueprint-editing-spec", function () {
+
+    var reelDocumentPromise, blueprint, a, b, defaultGroup;
+
+    beforeEach(function () {
+
+        Template._templateCache = {
+            moduleId: Object.create(null)
+        };
+
+        blueprint = new Blueprint();
+        a = new PropertyBlueprint().initWithNameBlueprintAndCardinality("a", blueprint, 1);
+        b = new PropertyBlueprint().initWithNameBlueprintAndCardinality("b", blueprint, 1);
+        blueprint.addPropertyBlueprint(a);
+        blueprint.addPropertyBlueprint(b);
+
+        defaultGroup = blueprint.addPropertyBlueprintGroupNamed("default");
+        defaultGroup.push(a, b);
+
+        reelDocumentPromise = mockReelDocument("foo/bar/mock.reel", {
+            "owner": {
+                "properties": {
+                    "element": {"#": "ownerElement"}
+                }
+            }
+        },
+        '<div id="ownerElement" data-montage-id="ownerElement"></div>')
+        .then(function (reelDocument) {
+            reelDocument.__ownerBlueprint = Promise(blueprint);
+            return reelDocument;
+        });
+    });
+
+    describe("adding a blueprint property", function () {
+
+        it("works", function () {
+            return reelDocumentPromise.then(function (reelDocument) {
+                var property = new PropertyBlueprint().initWithNameBlueprintAndCardinality("pass", blueprint, 1);
+                return reelDocument.addOwnerBlueprintProperty(property)
+                .then(function () {
+                    expect(defaultGroup.length).toBe(3);
+                    expect(defaultGroup[2].name).toBe("pass");
+                });
+            });
+        });
+
+        it("adds a default group if one doesn't exist", function () {
+            return reelDocumentPromise.then(function (reelDocument) {
+                blueprint = new Blueprint();
+                reelDocument.__ownerBlueprint = Promise(blueprint);
+
+                var property = new PropertyBlueprint().initWithNameBlueprintAndCardinality("pass", blueprint, 1);
+                return reelDocument.addOwnerBlueprintProperty(property)
+                .then(function () {
+                    defaultGroup = blueprint.propertyBlueprintGroupForName("default");
+                    expect(defaultGroup.length).toBe(1);
+                    expect(defaultGroup[0].name).toBe("pass");
+                });
+            });
+        });
+
+    });
+
+    describe("modifying a blueprint property", function () {
+        it("works", function () {
+            return reelDocumentPromise.then(function (reelDocument) {
+                return reelDocument.modifyOwnerBlueprintProperty(a, "valueType", "number")
+                .then(function () {
+                    expect(a.valueType).toBe("number");
+                });
+            });
+        });
+    });
+
+    describe("removing a blueprint property", function () {
+
+        it("works", function () {
+            return reelDocumentPromise.then(function (reelDocument) {
+                return reelDocument.removeOwnerBlueprintProperty(a)
+                .then(function () {
+                    expect(defaultGroup.length).toBe(1);
+                    expect(defaultGroup[0].name).toBe("b");
+                });
+            });
+
+        });
+
+    });
+});
--- test/ui/template-explorer/owner-cell/edit-properties-spec.js
+++ test/ui/template-explorer/owner-cell/edit-properties-spec.js
@@ -6,7 +6,7 @@ var PropertyBlueprint = require("montage/core/meta/property-blueprint").Property
 TestPageLoader.queueTest("edit-properties-test", function(testPage) {
     describe("ui/template-explorer/owner-cell/edit-properties", function() {
 
-        var test, editor, blueprint, defaultGroup;
+        var test, editor, blueprint, defaultGroup, editingDocument;
         beforeEach(function() {
             test = testPage.test;
 
@@ -22,24 +22,40 @@ TestPageLoader.queueTest("edit-properties-test", function(testPage) {
             defaultGroup.push(a, b);
 
             editor.ownerBlueprint = blueprint;
-            testPage.waitForDraw();
+
+            editingDocument = {
+                addOwnerBlueprintProperty: function () {},
+                modifyOwnerBlueprintProperty: function () {},
+                removeOwnerBlueprintProperty: function () {}
+            };
+
+            editor._ownerObject = {
+                editingDocument: editingDocument
+            };
+
+            testPage.waitForDraw(1, true);
         });
 
         describe("add property button", function () {
             it("does nothing if no name given", function () {
+                spyOn(editingDocument, "addOwnerBlueprintProperty");
+
                 expect(defaultGroup.length).toBe(2);
                 editor.templateObjects.addName.value = "";
                 testPage.clickOrTouch({target: editor.templateObjects.addProperty.element}, function () {
-                    expect(defaultGroup.length).toBe(2);
+                    expect(editingDocument.addOwnerBlueprintProperty).not.toHaveBeenCalled();
                 });
             });
 
-            it("adds to the default group", function () {
+            it("calls addOwnerBlueprintProperty to add the property", function () {
+                spyOn(editingDocument, "addOwnerBlueprintProperty");
+
                 expect(defaultGroup.length).toBe(2);
                 editor.templateObjects.addName.value = "pass";
                 testPage.clickOrTouch({target: editor.templateObjects.addProperty.element}, function () {
-                    expect(defaultGroup.length).toBe(3);
-                    expect(defaultGroup[2].name).toBe("pass");
+                    expect(editingDocument.addOwnerBlueprintProperty).toHaveBeenCalled();
+                    var arg = editingDocument.addOwnerBlueprintProperty.mostRecentCall.args[0];
+                    expect(arg.name).toBe("pass");
                 });
             });
 
@@ -50,27 +66,18 @@ TestPageLoader.queueTest("edit-properties-test", function(testPage) {
                     expect(editor.templateObjects.addName.value).toBe("");
                 });
             });
-
-            it("adds a default group if one doesn't exist", function () {
-                blueprint = new Blueprint();
-                editor.ownerBlueprint = blueprint;
-
-                editor.templateObjects.addName.value = "pass";
-                testPage.clickOrTouch({target: editor.templateObjects.addProperty.element}, function () {
-                    defaultGroup = blueprint.propertyBlueprintGroupForName("default");
-                    expect(defaultGroup.length).toBe(1);
-                    expect(defaultGroup[0].name).toBe("pass");
-                });
-            });
         });
 
         describe("remove button", function () {
             it("removes the property", function () {
+                spyOn(editingDocument, "removeOwnerBlueprintProperty");
+
                 runs(function () {
                     expect(defaultGroup.length).toBe(2);
                     testPage.clickOrTouch({target: editor.templateObjects.removeProperty[0].element}, function () {
-                        expect(defaultGroup.length).toBe(1);
-                        expect(defaultGroup[0].name).toBe("b");
+                        expect(editingDocument.removeOwnerBlueprintProperty).toHaveBeenCalled();
+                        var arg = editingDocument.removeOwnerBlueprintProperty.mostRecentCall.args[0];
+                        expect(arg.name).toBe("a");
                     });
                 });
             });
@@ -78,6 +85,8 @@ TestPageLoader.queueTest("edit-properties-test", function(testPage) {
 
         describe("valueType select", function () {
             it("changes to valueType of the property", function () {
+                spyOn(editingDocument, "modifyOwnerBlueprintProperty");
+
                 runs(function () {
                     expect(defaultGroup[0].valueType).toBe("string");
 
@@ -85,7 +94,11 @@ TestPageLoader.queueTest("edit-properties-test", function(testPage) {
                     editor.templateObjects.valueType[0].element.value = "number";
                     editor.templateObjects.valueType[0].handleChange();
 
-                    expect(defaultGroup[0].valueType).toBe("number");
+                    expect(editingDocument.modifyOwnerBlueprintProperty).toHaveBeenCalled();
+                    var args = editingDocument.modifyOwnerBlueprintProperty.mostRecentCall.args;
+                    expect(args[0].name).toBe("a");
+                    expect(args[1]).toBe("valueType");
+                    expect(args[2]).toBe("number");
                 });
             });
         });
