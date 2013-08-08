60e8400318831c77a650473e558686d93426aee8
Thibault Zanini <thibaultzanini@gmail.com>
2013-08-08 14:16:00 -0700

    Modified: dependencies List components
    - selectable
    - shows errors
    - can be removed
    - display a spinner when an action is performing.

---
+++ extensions/package-manager.filament-extension/ui/package-dependencies.reel/package-dependencies-group.reel/package-dependency-cell.reel/package-dependency-cell.js
@@ -0,0 +1,134 @@
+/**
+ * @module ui/package-dependency-cell.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component,
+    DEPENDENCY_TYPE_REGULAR = 'regular',
+    DETAILS_ERROR_LABEL = 'errors',
+    CAN_INSTALL_LABEL = 'Install',
+    DEFAULT_LABEL = '-';
+
+/**
+ * @class PackageDependencyCell
+ * @extends Component
+ */
+exports.PackageDependencyCell = Component.specialize(/** @lends PackageDependencyCell# */ {
+
+    constructor: {
+        value: function PackageDependencyCell() {
+            this.super();
+        }
+    },
+
+    _dependency: {
+        value: null
+    },
+
+    /**
+     * Cell's dependency
+     * @type {Object}
+     * @default null
+     */
+    dependency: {
+        set: function (module) {
+            if (module && typeof module === "object") {
+                this._dependency = module;
+                this.hasError = (this._dependency && Array.isArray(this._dependency.problems) && this._dependency.problems.length > 0);
+
+                 if (this._dependency.type !== DEPENDENCY_TYPE_REGULAR && this._dependency.missing) {
+                    this.canInstall = true;
+                } else {
+                    this.buttonLabel = DEFAULT_LABEL;
+                }
+            }
+        },
+        get: function () {
+            return this._dependency;
+        }
+    },
+
+    /**
+     * Dependency version or errors label.
+     * @type {String}
+     * @default null
+     */
+    details: {
+        value: null
+    },
+
+    _buttonLabel: {
+        value: null
+    },
+
+    /**
+     * Button action label.
+     * @type {String}
+     * @default null
+     */
+    buttonLabel: {
+        set: function (label) {
+            this._buttonLabel = (typeof label === 'string') ? label : DEFAULT_LABEL;
+        },
+        get: function () {
+            return this._buttonLabel;
+        }
+    },
+
+    _hasError: {
+        value: false
+    },
+
+    /**
+     * Returns true if the dependency shows some errors
+     * @type {boolean}
+     * @return {boolean}
+     */
+    hasError: {
+        set: function (bool) {
+            this._hasError = (typeof bool === 'boolean') ? bool : false;
+            this.details = (this.hasError) ? DETAILS_ERROR_LABEL : this.dependency.versionInstalled;
+        },
+        get: function () {
+            return this._hasError;
+        }
+    },
+
+    _performingAction: {
+        value: false
+    },
+
+    /**
+     * Displays a spinner if the dependency is either installing or removing.
+     * @type {boolean}
+     * @return {boolean}
+     */
+    performingAction: {
+        set: function (performing) {
+            this._performingAction = (typeof performing === 'boolean') ? performing : false ;
+        },
+        get: function () {
+            return this._performingAction;
+        }
+    },
+
+    _canInstall: {
+        value: false
+    },
+
+    /**
+     * Specifies if the dependency can be install directly from the list,
+     * Indeed, only the regular type is mandatory.
+     * @type {boolean}
+     * @return {boolean}
+     */
+    canInstall: {
+        set: function (bool) {
+            this._canInstall = (typeof bool === 'boolean') ? bool : false;
+            this.buttonLabel = (this._canInstall) ? CAN_INSTALL_LABEL : DEFAULT_LABEL;
+        },
+        get: function () {
+            return this._canInstall;
+        }
+    }
+
+});
--- extensions/package-manager.filament-extension/ui/package-dependencies.reel/package-dependencies.js
+++ extensions/package-manager.filament-extension/ui/package-dependencies.reel/package-dependencies.js
@@ -2,21 +2,130 @@
  * @module ui/package-dependencies.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Promise = require("montage/core/promise").Promise,
+    DEPENDENCY_TYPE_REGULAR = 'regular',
+    DEPENDENCY_TYPE_OPTIONAL = 'optional',
+    DEPENDENCY_TYPE_DEV = 'dev';
 
 /**
  * @class PackageDependencies
  * @extends Component
  */
 exports.PackageDependencies = Component.specialize(/** @lends PackageDependencies# */ {
+
     constructor: {
         value: function PackageDependencies() {
             this.super();
         }
     },
 
+    didDraw: {
+        value: function () {
+            this.addOwnPropertyChangeListener("selectedDependency", this, true);
+            this.addOwnPropertyChangeListener("selectedDependency", this, false);
+        }
+    },
+
+    /**
+     * Reference to the packageDocument.
+     * @type {Object}
+     * @default null
+     */
     editingDocument: {
         value: null
+    },
+
+    _selectedCell: {
+        value: null
+    },
+
+    /**
+     *  The current selected dependency.
+     * @type {Object}
+     * @default null
+     */
+    selectedCell: {
+        set: function (cell) {
+            this._selectedCell = cell;
+        },
+        get: function () {
+            return this._selectedCell;
+        }
+    },
+
+    /**
+     * Determines whether the future selected dependency belongs to the current selected list,
+     * if not then clear the current list selection.
+     * @function
+     * @param {Object} cell, represents the dependency which will be selected
+     */
+    handleSelectedDependencyWillChange: {
+        value: function (cell) {
+            if (this._selectedCell && cell && cell.type !== this._selectedCell.type) { // Needs manual change.
+                if (this._selectedCell.type === DEPENDENCY_TYPE_REGULAR) {
+                    this.templateObjects.packageDependenciesGroup.contentController.clearSelection();
+                } else if (this._selectedCell.type === DEPENDENCY_TYPE_OPTIONAL) {
+                    this.templateObjects.packageOptionalDependenciesGroup.contentController.clearSelection();
+                } else if (this._selectedCell.type === DEPENDENCY_TYPE_DEV) {
+                    this.templateObjects.packageDevDependenciesGroup.contentController.clearSelection();
+                }
+            }
+        }
+    },
+
+    /**
+     * Sets the current selected dependency.
+     * @function
+     * @param {Object} cell, represents the current selected dependency.
+     */
+    handleSelectedDependencyChange: {
+        value: function (cell) {
+            this.selectedCell = cell;
+        }
+    },
+
+    /**
+     * Sets the current selected dependency.
+     * @function
+     * @param {Object} cell, represents the future selected dependency.
+     * @private
+     */
+    _manualChange: {
+        value: function (cell) {
+            this.dispatchBeforeOwnPropertyChange('selectedDependency', cell);
+            this.dispatchOwnPropertyChange('selectedDependency', cell);
+        }
+    },
+
+    /**
+     * Sets the current selected dependency from the regular dependencies list.
+     * @function {Object}
+     */
+    dependencyCell: {
+        set: function (cell) {
+            this._manualChange(cell);
+        }
+    },
+
+    /**
+     * Sets the current selected dependency from the optional dependencies list.
+     * @type {Object}
+     */
+    optionalDependencyCell: {
+        set: function (cell) {
+            this._manualChange(cell);
+        }
+    },
+
+    /**
+     * Sets the current selected dependency from the dev dependencies list.
+     * @type {Object}
+     */
+    devDependencyCell: {
+        set: function (cell) {
+            this._manualChange(cell);
+        }
     }
 
 });
