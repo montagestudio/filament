9e5cda06196c338fbb330fef34903395127a4224
Thibault Zanini <thibaultzanini@gmail.com>
2013-08-09 15:40:24 -0700

    Modified: dependencies are not installed with a blank version anymore

--- extensions/package-manager.filament-extension/ui/search-modules.reel/search-modules.js
+++ extensions/package-manager.filament-extension/ui/search-modules.reel/search-modules.js
@@ -196,7 +196,7 @@ exports.SearchModules = Component.specialize(/** @lends SearchModules# */ {
             if (this.editingDocument && module && typeof module === 'object' && module.hasOwnProperty('name')) {
                 cell.installing(true);
 
-                var promise = this.editingDocument.installDependency(module.name).then(function (data) {
+                var promise = this.editingDocument.installDependency(module.name, module.version).then(function (data) {
                     if (data && typeof data === 'object' && data.hasOwnProperty('name') && module.name === data.name) {
                         if (cell) {
                             module.installed = true;
