cda470ec757d0607b938f563a67983d15805b772
Thibault Zanini <thibaultzanini@gmail.com>
2013-09-17 14:32:15 -0700

    Add LumieresDB object
    Checks if the database is installed.
    Can init the database.
    Affords a way to get an instance of the database.

---
+++ backend_plugins/package-manager-library/lumieres_database.js
@@ -0,0 +1,88 @@
+/*global __dirname */
+var sqlite3 = require('sqlite3'),
+    FS = require("q-io/fs"),
+    path = require('path'),
+    Q = require('q'),
+    INIT_FILENAME = 'init_database.sql',
+    DATABASE_NAME = 'lumieres_sqlite.db';
+
+var LumieresDB = Object.create(Object.prototype, {
+
+    init: {
+        value: function () {
+            var self = this;
+
+            return FS.read(path.join(__dirname, INIT_FILENAME)).then(function (file) {
+                return Q.ninvoke(self.database, "exec", file).then(function () {
+                    return self.database;
+                });
+            });
+        }
+    },
+
+    load: {
+        value: function (url) {
+            if (typeof url === "string" && url.length > 0) {
+                this.url = path.join(url, DATABASE_NAME);
+                return this._getInstance();
+            }
+
+            return Q.reject(new Error("The Database path is incorrect"));
+        }
+    },
+
+    _getInstance: {
+        value: function () {
+            var self = this;
+
+            return FS.exists(this.url).then(function (exist) {
+                return Q.fcall(function () {
+                    self.database = new sqlite3.Database(self.url);
+
+                    if (!exist) {
+                        return self.init();
+                    }
+                    return self.database;
+                });
+            });
+        }
+    },
+
+    open: {
+        value: function () {
+            if (!this.url) {
+                throw new Error("LumieresDB should be loaded first");
+            }
+
+            if (!this.database || !this.database.open) {
+                return this._getInstance();
+            }
+            return Q(this.database); // already opened
+        }
+    },
+
+    close: {
+        value: function () {
+            return Q.ninvoke(this.database, "close");
+        }
+    },
+
+    database: {
+        value: null,
+        writable: true
+    },
+
+    url: {
+        value: null,
+        writable: true
+    },
+
+    opened: {
+        get: function () {
+            return (this.database && this.database.open);
+        }
+    }
+
+});
+
+exports.LumieresDB = LumieresDB;
