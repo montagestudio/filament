8070e14268b472f0c95648bae7516c06cf4e4eb4
Thibault Zanini <thibaultzanini@gmail.com>
2013-09-17 14:33:36 -0700

    Add PackageManagerRegistry
    Allows to manage the PACKAGE_MANAGER_REGISTRY table, which store every
    packages from the npm registry.

---
+++ backend_plugins/package-manager-library/package_manager_registry.js
@@ -0,0 +1,101 @@
+var LumieresDB = require("./lumieres_database").LumieresDB,
+    npm = require("npm"),
+    Q = require('q'),
+    TIMEOUT_BEFORE_NEW_REQUEST = 900; // seconds
+
+var PackageManagerRegistry = Object.create(Object.prototype, {
+
+    _getLastUpdate: {
+        value: function () {
+            if (!this.lastUpdate) {
+                var self = this;
+
+                return LumieresDB.open().then(function (instance) {
+                    return Q.ninvoke(instance, "get", "SELECT KEY_VALUE AS lastUpdate " +
+                            "FROM PACKAGE_MANAGER_DATA " +
+                            "WHERE KEY_NAME = 'REGISTRY_CACHE'")
+                        .then(function (row) {
+                            return LumieresDB.close().then(function () {
+                                self.lastUpdate = Number(row ? row.lastUpdate : 0);
+                                return self.lastUpdate;
+                            });
+                        });
+                });
+            }
+
+            return Q(this.lastUpdate);
+        }
+    },
+
+    lastUpdate: {
+        value: null,
+        writable: true
+    },
+
+    update: {
+        value: function (all) {
+            var self = this;
+
+            return this._getLastUpdate().then(function (lastUpdate) {
+                if ((Date.now() - lastUpdate) < (TIMEOUT_BEFORE_NEW_REQUEST * 1000)) { // uses cache
+                    return Q(true);
+                }
+
+                // Request for the npm registry
+                var request = (!!all || lastUpdate === 0) ? "/-/all" : "/-/all/since?stale=update_after&startkey=" + lastUpdate;
+
+                return LumieresDB.open().then(function (instance) {
+                    if (!npm.config.loaded) {
+                        throw new Error("NPM should be loaded first");
+                    }
+
+                    return Q.ninvoke(npm.registry, "get", request, TIMEOUT_BEFORE_NEW_REQUEST, false, true).then(function (modules) {
+                        return Q.ninvoke(instance, "serialize").then(function () {
+                            return Q.ninvoke(instance, "run", "BEGIN").then(function () {
+                                var keys = Object.keys(modules),
+                                    stmt = instance.prepare(
+                                        "INSERT INTO " +
+                                            "PACKAGE_MANAGER_REGISTRY (NAME, VERSION, KEYWORDS, AUTHOR, DESCRIPTION) " +
+                                            "VALUES (?, ?, ?, ?, ?)"
+                                    );
+
+                                for (var i = 0, length = keys.length; i < length; i++) {
+                                    var module = modules[keys[i]],
+                                        version = (module.versions && typeof module.versions === "object") ?
+                                            Object.keys(module.versions)[0] : null;
+
+                                    if (module.name && version) {
+                                        stmt.run(
+                                            module.name,
+                                            version,
+                                            JSON.stringify(module.keywords),
+                                            JSON.stringify(module.author),
+                                            module.description
+                                        );
+                                    }
+                                }
+
+                                return Q.ninvoke(stmt, "finalize").then(function () {
+                                    return Q.ninvoke(instance, "run", "COMMIT").then(function () {
+                                        var updated = Date.now();
+
+                                        return Q.ninvoke(instance, "run", "UPDATE PACKAGE_MANAGER_DATA " +
+                                                "SET KEY_VALUE = ? " +
+                                                "WHERE KEY_NAME = 'REGISTRY_CACHE'", updated)
+                                            .then(function () {
+                                                self.lastUpdate = updated;
+                                                return LumieresDB.close();
+                                            });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        }
+    }
+
+});
+
+exports.PackageManagerRegistry = PackageManagerRegistry;
