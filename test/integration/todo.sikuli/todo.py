bc2883a6ab13d82fc54e1e240ae271e258566da5
Mike Czepiel <mike@declarativ.com>
2013-10-03 01:15:17 -0700

    Update todo test for new UI
    Well there are enough glitches that this doesn't currently actually
    pass. But I've updated some of it; I'll fix up the listener portion
    once this gets running.

--- test/integration/todo.sikuli/todo.py
+++ test/integration/todo.sikuli/todo.py
@@ -13,7 +13,7 @@ templateExplorer = lumieresRegion.find("1376294075874.png")
 
 digitPackageRegion = find("1372142584813.png")
 # Add a title
-dragDrop(digitPackageRegion.find(Pattern("1372142022209.png").similar(0.81)), "1371600526337.png")
+lumieres.insertDomObject(digitPackageRegion.find(Pattern("1372142022209.png").similar(0.81)), "1380786943201.png", "child")
 
 wait(Pattern("1371600653629.png").targetOffset(20,12), 3)
 doubleClick(getLastMatch())
@@ -35,12 +35,13 @@ rangeControllerInspector.click(Pattern("1371770852843.png").targetOffset(59,13))
 
 # Add a list to present the tasks
 
-dragDrop(digitPackageRegion.find("1372142855514.png"), Pattern("1379348761930.png").targetOffset(-73,27))
+lumieres.insertDomObject(digitPackageRegion.find("1372142855514.png"), "1380787051214.png", "next")
 
 #Set the list to receive its content from the rangeController we created
 dragDrop(Pattern("1379357122124.png").similar(0.51), Pattern("1379357187665.png").targetOffset(33,59))
 
 #Remove the placeholder list content
+hover("1380787922104.png")
 click(Pattern("1379348525259.png").similar(0.54).targetOffset(43,0))
 
 # Create a component to encapsulate the presentation of a task in the list
@@ -56,7 +57,7 @@ sleep(1)
 explorer.wait("1371626266866.png", 1)
 wait("1376294233084.png", 2)
 #Add a checkbox to complete tasks
-dragDrop("1371626348331.png", "1371626362160.png")
+lumieres.insertDomObject("1371626348331.png", "1380787145595.png", "child")
 checkboxCard = find("1376294261916.png")
 
 # Bind the checkbox to the task's completed state
@@ -64,7 +65,7 @@ checkboxCard.inside().find("1376294281054.png").right().click("1376294297971.png
 lumieres.bind("checked", False, "@owner.task.completed")
 
 # Add a textfiled to edit the title of the task
-dragDrop("1372143026732.png", Pattern("1379357399537.png").targetOffset(-109,21))
+lumieres.insertDomObject("1372143026732.png", "1380787195094.png", "next")
 
 taskTitleCard = find("1376294350349.png")
 taskTitleCard.inside().find("1376294281054.png").right().click("1376294297971.png")
@@ -81,7 +82,7 @@ click("1371628279454.png")
 
 sleep(2)
 #add our task component to the list
-dragDrop(Pattern("1371628340381.png").similar(0.73).targetOffset(1,-6), Pattern("1371632573099.png").targetOffset(-33,10))
+lumieres.insertDomObject(Pattern("1371628340381.png").similar(0.73).targetOffset(1,-6), "1380787254026.png", "child")
 sleep(1)
 
 taskCard = find("1376295091372.png")
@@ -93,7 +94,7 @@ lumieres.bind("task", True, "@list1.objectAtCurrentIteration")
 lumieres.save()
 
 #Add a new task button
-dragDrop("1371708733359.png", Pattern("1379359175062.png").targetOffset(-47,0))
+lumieres.insertDomObject("1371708733359.png", "1380787319604.png", "next")
 doubleClick("1371708858768.png")
 type("New Task")
 
@@ -115,7 +116,7 @@ dragDrop(buttonEvent, actionListenerCard)
 click("1379358296330.png")
 
 #add a badge to count the remaining tasks
-dragDrop("1371711022203.png", Pattern("1379359212690.png").targetOffset(-49,2))
+lumieres.insertDomObject("1371711022203.png", "1380787370986.png", "next")
 
 badgeCard = find("1376295271102.png")
 badgeCard.find("1376294281054.png").right().click("1376294297971.png")
