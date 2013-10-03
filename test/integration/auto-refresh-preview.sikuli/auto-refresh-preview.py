7e420e52439fcf7c49a04ca424465fbe2d4202c1
Mike Czepiel <mike@declarativ.com>
2013-10-02 23:45:55 -0700

    Update auto-refresh test for new domExplorer UX
    Among some other little fixes to make sure this works right

--- test/integration/auto-refresh-preview.sikuli/auto-refresh-preview.py
+++ test/integration/auto-refresh-preview.sikuli/auto-refresh-preview.py
@@ -9,6 +9,7 @@ click(Pattern("1379635114233.png").targetOffset(62,2))
 browserLaunched = False
 maxAttemptCount = 10
 attemptCount = 0
+browserRegion = None
 
 while not browserLaunched or attemptCount > maxAttemptCount:
     sleep(1)
@@ -19,9 +20,18 @@ while not browserLaunched or attemptCount > maxAttemptCount:
             lumieresRegion.h != browserRegion.h or
             lumieresRegion.x != browserRegion.x or
             lumieresRegion.y != browserRegion.y)
-    
+    if (not browserLaunched):
+        browserRegion = None
+
+if (not browserRegion):
+    raise Exception("Could not find Browser")
+
 type(Key.TAB, Key.META)
 
-dragDrop("1379636068715.png", "1379636076570.png")
+#Edit the applications
+lumieres.insertDomObject("1379636068715.png", "1379636076570.png", "child")
 lumieres.save()
-wait(browserRegion.find("1379636165608.png"))
+
+# Meanwhile, back in the browser we should see the result
+type(Key.TAB, Key.META)
+browserRegion.wait("1379636165608.png", 1)
