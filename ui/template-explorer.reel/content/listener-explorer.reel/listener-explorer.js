a62e9d8e63cb35b4383630b01304b5291d3e72c9
cesine <cesine@yahoo.com>
2013-10-21 16:43:27 -0400

    drag listener to copy to another component

--- ui/template-explorer.reel/content/listener-explorer.reel/listener-explorer.js
+++ ui/template-explorer.reel/content/listener-explorer.reel/listener-explorer.js
@@ -3,6 +3,7 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component,
+    ObjectLabelConverter = require("core/object-label-converter").ObjectLabelConverter,
     MimeTypes = require("core/mime-types");
 
 /**
@@ -19,6 +20,31 @@ exports.ListenerExplorer = Component.specialize(/** @lends ListenerExplorer# */ 
 
     _willAcceptDrop: {
         value: false
+    },
+
+    acceptsDrop: {
+        value: function(event) {
+            return this.acceptsListenerDrop(event) || this.acceptsListenerLabelDrop(event);
+        }
+    },
+
+    acceptsListenerLabelDrop: {
+        value: function(event) {
+            var availableTypes = event.dataTransfer.types,
+                target = event.target,
+                element = this.element;
+
+            return availableTypes && availableTypes.has(MimeTypes.SERIALIZATION_OBJECT_LABEL) && (target === element || element.contains(target));
+        }
+    },
+
+    acceptsListenerDrop: {
+        value: function(event) {
+            var availableTypes = event.dataTransfer.types,
+                plusButton = this.element.querySelector("[data-montage-id=addListenerButton]");
+
+            return availableTypes && availableTypes.has(MimeTypes.MONTAGE_LISTENER) && plusButton && event.target === plusButton;
+        }
     },
 
     enterDocument: {
@@ -41,21 +67,15 @@ exports.ListenerExplorer = Component.specialize(/** @lends ListenerExplorer# */ 
 
     handleDragover: {
         value: function (event) {
-            var availableTypes = event.dataTransfer.types,
-                target = event.target,
-                element = this.element;
-
-            if (!availableTypes) {
+            if (!this.acceptsDrop(event)) {
                 event.dataTransfer.dropEffect = "none";
                 this._willAcceptDrop = false;
-            } else if (availableTypes.has(MimeTypes.SERIALIZATION_OBJECT_LABEL) && (target === element || element.contains(target))) {
-
-                // allows us to drop
-                event.preventDefault();
-                event.stopPropagation();
-                event.dataTransfer.dropEffect = "link";
-                this._willAcceptDrop = true;
+                return;
             }
+
+            event.stop();
+            event.dataTransfer.dropEffect = this.acceptsListenerDrop(event) ? "copy" : "link";
+            this._willAcceptDrop = true;
         }
     },
 
@@ -68,14 +88,14 @@ exports.ListenerExplorer = Component.specialize(/** @lends ListenerExplorer# */ 
     handleDrop: {
         value: function (event) {
             var availableTypes = event.dataTransfer.types,
-                target = event.target,
-                element = this.element,
                 listenerModel;
 
-            if (availableTypes.has(MimeTypes.SERIALIZATION_OBJECT_LABEL) && (target === element || element.contains(target))) {
+            if (!this.acceptsDrop(event)) {
+                return;
+            }
 
-                event.stopPropagation();
-                var listenerLabel= event.dataTransfer.getData(MimeTypes.SERIALIZATION_OBJECT_LABEL);
+            if (this.acceptsListenerLabelDrop(event)) {
+                var listenerLabel = event.dataTransfer.getData(MimeTypes.SERIALIZATION_OBJECT_LABEL);
                 listenerModel = Object.create(null);
                 listenerModel.targetObject = this.templateObject;
                 listenerModel.listener = this.templateObject.editingDocument.editingProxyMap[listenerLabel];
@@ -83,6 +103,21 @@ exports.ListenerExplorer = Component.specialize(/** @lends ListenerExplorer# */ 
                 this.dispatchEventNamed("addListenerForObject", true, false, {
                     listenerModel: listenerModel
                 });
+            } else if (this.acceptsListenerDrop(event)) {
+                var data = event.dataTransfer.getData(MimeTypes.MONTAGE_LISTENER),
+                    targetObject = this.templateObject,
+                    // TODO: security issues?
+                    transferObject = JSON.parse(data),
+                    editingDocument = this.ownerComponent.ownerComponent.editingDocument,
+                    objectLabelConverter,
+                    listener;
+                if (!transferObject.listenerLabel) {
+                    return;
+                }
+                objectLabelConverter = new ObjectLabelConverter();
+                objectLabelConverter.editingDocument = editingDocument;
+                listener = objectLabelConverter.revert(transferObject.listenerLabel);
+                editingDocument.addOwnedObjectEventListener(targetObject, transferObject.type, listener, transferObject.useCapture, transferObject.methodName);
             }
 
             this._willAcceptDrop = false;
