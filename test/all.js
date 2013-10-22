d1d48591ff9b2ae6f4ed50e8de451642230384b4
Stuart Knightley <stuart@declarativ.com>
2013-10-21 20:05:27 -0700

    File descriptor specs moved into lumieres

--- test/all.js
+++ test/all.js
@@ -1,9 +1,10 @@
+require.inject("adaptor/client/core/file-descriptor", require("./mocks/file-descriptor-mocks"));
+
 require("montage-testing").run(require, [
     // Please keep in alphabetical order
     "extensions/package-manager.filament-extension/test/package-document-spec.js",
     "extensions/package-manager.filament-extension/test/package-queue-manager-spec.js",
     "extensions/package-manager.filament-extension/test/package-tools-spec.js",
-    "test/core/file-descriptor-spec",
     "test/core/node-proxy-spec",
     "test/core/object-references-spec",
     "test/core/project-controller-extensions-spec",
