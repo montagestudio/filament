44bcffaf2678026e0fd80981a7391f0943fb2add
Mike Czepiel <mike@declarativ.com>
2013-11-16 17:12:03 -0800

    Add some environment specific code to app delegate
    Nothing actually happening just yet as I need to reorganize the 
    base app-delegate

--- core/firefly-application-delegate.js
+++ core/firefly-application-delegate.js
@@ -6,6 +6,21 @@ exports.FireflyApplicationDelegate = ApplicationDelegate.specialize({
         value: function ApplicationDelegate () {
             this.super();
         }
+    },
+
+    isBareRepository: {
+        value: false
+    },
+
+    isPreparingProjectWorkspace: {
+        value: false
+    },
+
+    handleInitializeRepository: {
+        value: function () {
+            //TODO only if the repository exists, and is bare: go an initialize it with the result of minit create
+            debugger
+        }
     }
 
 });
