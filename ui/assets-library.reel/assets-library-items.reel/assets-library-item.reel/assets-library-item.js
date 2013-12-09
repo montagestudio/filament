a943a33c0b45dfb3c2b7439a1a1a810a52575d99
Thibault Zanini <thibaultzanini@gmail.com>
2013-12-09 17:55:16 +0100

    Add Asset Library UI.

---
+++ ui/assets-library.reel/assets-library-items.reel/assets-library-item.reel/assets-library-item.js
@@ -0,0 +1,45 @@
+/**
+ * @module ui/assets-library-item.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component,
+    MimeTypes = require("core/mime-types");
+
+/**
+ * @class AssetsLibraryItem
+ * @extends Component
+ */
+exports.AssetsLibraryItem = Component.specialize(/** @lends AssetsLibraryItem# */ {
+
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this._element.addEventListener("dragstart", this, true);
+            }
+        }
+    },
+
+    prototypeAsset: {
+        value: null
+    },
+
+    captureDragstart: {
+        value: function (event) {
+            var dataTransfer = event.dataTransfer;
+
+            if (dataTransfer) {
+                dataTransfer.effectAllowed = "copyMove";
+                dataTransfer.setData("text/plain", this.prototypeAsset.fileUrl);
+
+                var iconElement = this.templateObjects.prototypeAssetIcon.element;
+
+                dataTransfer.setDragImage(
+                    iconElement,
+                    iconElement.width / 2,
+                    iconElement.height / 2
+                );
+            }
+        }
+    }
+
+});
