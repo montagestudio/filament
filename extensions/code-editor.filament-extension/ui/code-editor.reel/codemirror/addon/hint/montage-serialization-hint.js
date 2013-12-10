c878095b3e741d9d02fd7a74267aa7460e1e857b
António Afonso <antonio.afonso@gmail.com>
2013-12-09 19:17:09 -0800

    Add serialization units autocomplete

--- extensions/code-editor.filament-extension/ui/code-editor.reel/codemirror/addon/hint/montage-serialization-hint.js
+++ extensions/code-editor.filament-extension/ui/code-editor.reel/codemirror/addon/hint/montage-serialization-hint.js
@@ -1,12 +1,14 @@
 (function () {
     var Pos = CodeMirror.Pos;
 
+    var serializationUnits = ["prototype", "object", "properties", "bindings", "listeners"];
+
     function serializationHint(editor, options) {
         var cursor = editor.getCursor(),
             token = editor.getTokenAt(cursor);
 
-        if (token.type === "module") {
-            var hasEndQuote = token.string.slice(-1) === '"';
+        if (token.type === "module" || token.type === "unit") {
+            var hasEndQuote = token.string.slice(-1) === '"' && token.string.length > 1;
             token = {
                 // strip the quotes
                 start: token.start + 1,
@@ -31,6 +33,8 @@
 
         if (token.type === "module") {
             symbols = options.serializationModules;
+        } else if (token.type === "unit") {
+            symbols = serializationUnits;
         } else {
             return [];
         }
