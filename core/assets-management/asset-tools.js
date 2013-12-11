fbe4d1421c5e7e4234bc3108fbb86a76d81a4b3a
Thibault Zanini <thibaultzanini@gmail.com>
2013-12-11 11:21:07 +0100

    Comments for AssetTools

--- core/assets-management/asset-tools.js
+++ core/assets-management/asset-tools.js
@@ -1,7 +1,17 @@
 var AssetsConfig = require("./assets-config").AssetsConfig;
 
+// Provides some functions to the Asset Management.
 var AssetTools = {
 
+    /**
+     * Decomposes a string (a file url),
+     * in order to get its filename, its name and its extension
+     * ex: /a/b/c/foo.bar => {filename: foo.bar, name: foo, extension: bar}
+     * @function
+     * @public
+     * @param {String} fileUrl - a file Url.
+     * @return {Object}
+     */
     defineFileDataWithUrl: function (fileUrl) {
         if (this.isFileUrlValid(fileUrl)) {
             var fileData = /([^\/]+)\.([^\.]+)$|([^\/]+)$/.exec(fileUrl);
@@ -18,14 +28,36 @@ var AssetTools = {
         return null;
     },
 
+    /**
+     * Checks if a string is an url valid.
+     * @function
+     * @public
+     * @param {String} fileUrl - a file Url.
+     * @return {Boolean}
+     */
     isFileUrlValid: function (fileUrl) {
         return typeof fileUrl === 'string' && fileUrl.length > 0 && fileUrl.charAt(fileUrl.length-1) !== '/';
     },
 
+    /**
+     * Checks if an Asset Category is supported.
+     * @function
+     * @public
+     * @param {String} assetCategory - a supported Asset Category.
+     * @return {Boolean}
+     */
     isAssetCategoryValid: function (assetCategory) {
         return typeof assetCategory === 'string' && assetCategory.length > 0 && AssetsConfig.assetCategories.hasOwnProperty(assetCategory);
     },
 
+    /**
+     * Checks if a mime-type is supported.
+     * @function
+     * @public
+     * @param {String} mimeType - a supported mime-type.
+     * @param {String} [assetCategory] - an Asset Category.
+     * @return {Boolean}
+     */
     isMimeTypeSupported: function (mimeType, assetCategory) {
         var assetCategories =  AssetsConfig.assetCategories,
             self = this;
@@ -39,10 +71,27 @@ var AssetTools = {
         });
     },
 
+    /**
+     * Checks if an Asset Object is valid,
+     * or in other words its file url and its mime-type.
+     * @function
+     * @public
+     * @param {Object} asset - an Asset Object.
+     * @param {Object} asset.fileUrl - a file Url.
+     * @param {Object} asset.mimeType - a supported mime-type.
+     * @return {Boolean}
+     */
     isAssetValid: function (asset) {
         return asset && typeof asset && this.isFileUrlValid(asset.fileUrl) && this.isMimeTypeSupported(asset.mimeType);
     },
 
+    /**
+     * Checks if a mime-type is supported.
+     * @function
+     * @public
+     * @param {String} mimeType - a supported mime-type.
+     * @return {Object}
+     */
     findAssetCategoryFromMimeType: function (mimeType) {
         if (typeof mimeType === 'string' && mimeType.length > 0) {
             var dataMimeType = mimeType.split('/');
