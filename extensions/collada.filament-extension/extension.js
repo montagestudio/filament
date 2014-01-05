971609e052ae43131a415fe7da82e012f5c74587
Mike Czepiel <mike@declarativ.com>
2014-01-04 22:17:17 -0800

    Migrate collada extension to new libraryItems

--- extensions/collada.filament-extension/extension.js
+++ extensions/collada.filament-extension/extension.js
@@ -1,9 +1,6 @@
-var CoreExtension = require("filament-extension/core/extension").Extension,
-    Promise = require("montage/core/promise").Promise,
-    libraryItems = require("library-items").libraryItems,
-    libraryAdditions = require("library-items").libraryAdditions;
+var CoreExtension = require("filament-extension/core/extension").Extension;
 
-exports.Extension = CoreExtension.specialize( {
+var Extension = exports.Extension = CoreExtension.specialize( {
 
     constructor: {
         value: function Extension() {
@@ -13,32 +10,16 @@ exports.Extension = CoreExtension.specialize( {
 
     activate: {
         value: function (application, projectController) {
-
-            Object.keys(libraryItems).forEach(function (moduleId) {
-                projectController.registerLibraryItemForModuleId(libraryItems[moduleId], moduleId);
-            });
-
-            Object.keys(libraryAdditions).forEach(function (moduleId) {
-                projectController.addLibraryItemWithModuleIdForPackage(libraryAdditions[moduleId], moduleId, "glTF-webgl-viewer");
-            });
-
-            return Promise.resolve(this);
+            return this.installLibraryItems(projectController, "glTF-webgl-viewer").thenResolve(this);
         }
     },
 
     deactivate: {
         value: function (application, projectController) {
-
-            Object.keys(libraryItems).forEach(function (moduleId) {
-                projectController.unregisterLibraryItemForModuleId(moduleId);
-            });
-
-            Object.keys(libraryAdditions).forEach(function (moduleId) {
-                projectController.removeLibraryItemWithModuleIdForPackage(libraryAdditions[moduleId], moduleId, "glTF-webgl-viewer");
-            });
-
-            return Promise.resolve(this);
+            return this.uninstallLibraryItems(projectController, "glTF-webgl-viewer").thenResolve(this);
         }
     }
 
 });
+
+Extension.packageLocation = require.location;
