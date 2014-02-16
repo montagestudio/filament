0d984c66f48bb76997017cda56d0e62d9d5fdeb7
Thibault Zanini <thibaultzanini@gmail.com>
2014-02-16 11:29:28 +0100

    Add DependencyState Object
    Specify the different state of a dependency, pendingInstall,
    pendingRemoval, hasError, hasUpdate, AcceptInstall

--- extensions/package-manager.filament-extension/core/dependency.js
+++ extensions/package-manager.filament-extension/core/dependency.js
@@ -1,8 +1,19 @@
+var Montage = require("montage").Montage,
+
+    DEPENDENCY_STATE_LABELS = {
+        HAS_ERRORS: "errors",
+        HAS_UPDATE: "update available",
+        PENDING_REMOVAL: "pending removal",
+        PENDING_INSTALL: "pending install",
+        ACCEPT_INSTALL: "not installed"
+    };
+
 var Dependency = function Dependency(name, version, type) {
     this.name = name;
     this.version = version;
     this.type = type;
     this.missing = true;
+    this.state = new DependencyState(this);
 };
 
 Object.defineProperties(Dependency, {
@@ -34,4 +45,103 @@ Object.defineProperties(Dependency, {
 
 });
 
+var DependencyState = Montage.specialize({
+
+    constructor: {
+        value: function DependencyStates(dependency) {
+            this.super();
+
+            if (!dependency) {
+                throw new Error("Cannot instantiate a new DependencyStates object with:" + dependency);
+            }
+
+            this._dependency = dependency;
+
+            this.addPathChangeListener("_dependency.update", this, "_updateStateMessage");
+        }
+    },
+
+    _dependency: {
+        value: null
+    },
+
+    _pendingRemoval: {
+        value: null
+    },
+
+    pendingRemoval: {
+        set: function (flag) {
+            this._pendingRemoval = !!flag;
+            this._pendingInstall = false;
+
+            this._updateStateMessage();
+        },
+        get: function () {
+            return this._pendingRemoval;
+        }
+    },
+
+    _pendingInstall: {
+        value: null
+    },
+
+    pendingInstall: {
+        set: function (flag) {
+            this._pendingInstall = !!flag;
+            this._pendingRemoval = false;
+
+            this._updateStateMessage();
+        },
+        get: function () {
+            return this._pendingInstall;
+        }
+    },
+
+    hasErrors: {
+        get: function () {
+            var module = this._dependency;
+            return module && Array.isArray(module.problems) && module.problems.length > 0;
+        }
+    },
+
+    hasUpdate: {
+        get: function () {
+            return this._dependency && this._dependency.update;
+        }
+    },
+
+    acceptInstall: {
+        get: function () {
+            var module = this._dependency;
+            return module && module.type !== "dependencies" && module.missing && !this.pendingInstall;
+        }
+    },
+
+    _updateStateMessage: {
+        value: function () {
+            var message = null;
+
+            if (this.pendingRemoval) {
+                message = DEPENDENCY_STATE_LABELS.PENDING_REMOVAL;
+            } else if (this.pendingInstall) {
+                message = DEPENDENCY_STATE_LABELS.PENDING_INSTALL;
+            } else if (this.hasErrors) {
+                message = DEPENDENCY_STATE_LABELS.HAS_ERRORS;
+            } else if (this.hasUpdate) {
+                message = DEPENDENCY_STATE_LABELS.HAS_UPDATE;
+            } else if (this.acceptInstall) {
+                message = DEPENDENCY_STATE_LABELS.ACCEPT_INSTALL;
+            }
+
+            this.message = message;
+        }
+    },
+
+    message: {
+        value: null
+    }
+});
+
+
 exports.Dependency = Dependency;
+exports.DependencyState = DependencyState;
