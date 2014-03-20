5a88f58ad55d7c0fe27c1dfc8f4e30cbf185baf4
António Afonso <antonio.afonso@gmail.com>
2014-03-20 08:51:48 -0700

    Add download build chain

---
+++ extensions/build-download.filament-extension/extension.js
@@ -0,0 +1,81 @@
+var CoreExtension = require("filament-extension/core/extension").Extension,
+    Promise = require("montage/core/promise").Promise,
+    MenuItem = require("filament/adaptor/client/core/menu-item").MenuItem;
+
+exports.Extension = CoreExtension.specialize( {
+
+    constructor: {
+        value: function Extension() {
+            this.super();
+        }
+    },
+
+    name: {
+        get:function () {
+            //TODO read the name from the package or something
+            return "Build Download";
+        }
+    },
+
+    application: {
+        value:null
+    },
+
+    projectController: {
+        value:null
+    },
+
+    activate: {
+        value: function (application, projectController) {
+            var self = this;
+            this.application = application;
+            this.projectController = projectController;
+
+            projectController.addPathChangeListener("projectDocument", function(projectDocument) {
+                if (projectDocument) {
+                    projectDocument.build.addChain("download",
+                        "Download",
+                        null,
+                        [{
+                            thisp: self,
+                            performBuildStep: self.archive
+                        }, {
+                            thisp: self,
+                            performBuildStep: self.downloadArchive
+                        }]);
+                }
+            });
+        }
+    },
+
+    deactivate: {
+        value: function (application, projectController) {
+            if (projectController.projectDocument) {
+                projectController.projectDocument.build.removeChain("download");
+            }
+            this.application = null;
+            this.projectController = null;
+        }
+    },
+
+    archive: {
+        value: function(options) {
+            // TODO: what is the right way to get the environment bridge?
+            var bridge = this.application.delegate.environmentBridge;
+
+            options.updateStatusMessage("Archiving...");
+            return bridge.buildArchive();
+        }
+    },
+
+    downloadArchive: {
+        value: function() {
+            // TODO: what is the right way to get the environment bridge?
+            var bridge = this.application.delegate.environmentBridge;
+            var repositoryController = bridge.repositoryController;
+            var url = "/build/" + repositoryController.owner + "/" + repositoryController.repo + "/archive";
+
+            return bridge.downloadFile(url);
+        }
+    }
+});
