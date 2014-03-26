9d6145d8b5d3ef4069a98db89491255cf3b38c78
Jean-Francois Ducarroz <github@ducarroz.org>
2014-03-26 11:04:26 -0700

    Add merge and updateProjectRefs API

--- core/project-document.js
+++ core/project-document.js
@@ -256,9 +256,9 @@ exports.ProjectDocument = Document.specialize({
                 self = this,
                 updatePromise;
 
-            this.isBusy = true;
-
             if (bridge && typeof bridge.listRepositoryBranches === "function") {
+                this.isBusy = true;
+
                 updatePromise = bridge.listRepositoryBranches()
                     .then(function (response) {
                         var branches = self.branches = response.branches;
@@ -269,7 +269,22 @@ exports.ProjectDocument = Document.specialize({
                             //jshint +W106
                         }
 
-                        return self._updateShadowDelta().thenResolve(response);
+                        return self._updateShadowDelta()
+                        .then(function(shadowStatus) {
+                            var refs = [shadowStatus.localParent, shadowStatus.remoteParent, shadowStatus.remoteShadow],
+                                needSync = false;
+
+                            refs.some(function(status) {
+                                if (status.ahead > 0 || status.behind > 0) {
+                                    needSync = true;
+                                    return true;
+                                }
+                            });
+
+                            if (needSync) {
+                                return self._updateProjectRefs();
+                            }
+                        }).thenResolve(response);
                     })
                     .finally(function (result) {
                         self.isBusy = false;
@@ -295,6 +310,50 @@ exports.ProjectDocument = Document.specialize({
                         self.behindCount = shadowStatus.localParent.behind;
                         return shadowStatus;
                     });
+            }
+
+            return Promise(result);
+        }
+    },
+
+    _updateProjectRefs: {
+        value: function(resolution) {
+            var bridge = this._environmentBridge,
+                result;
+
+            if (bridge && bridge.updateProjectRefs && typeof bridge.updateProjectRefs === "function") {
+                result = bridge.updateProjectRefs(resolution)
+                .then(function(result) {
+                    var notifications = result.notifications,
+                        nbrNotification = notifications ? notifications.length : 0,
+                        resolutionStrategy = result.resolutionStrategy;
+
+                    if (result.success === false && nbrNotification && resolutionStrategy.indexOf("rebase") !== -1) {
+                        // Automatically sync the shadow branch with its remote counter part or parent
+                        return bridge.updateProjectRefs("rebase");
+                    }
+                    return result;
+                })
+                .then(function(result) {
+                    var notifications = result.notifications,
+                       nbrNotification = notifications ? notifications.length : 0,
+                       lastNotification = notifications[nbrNotification - 1];
+
+                    // jshint noempty:false
+                    if (result.success !== true) {
+                        if (lastNotification.type === "shadowsOutOfSync") {
+                            // Local shadow has diverged from remote shadow (not a rebase case)
+                            // TODO: ask user what to do in case of conflict
+                            /* TODO: Possible options to propose to user
+                                1. Discard remote changes  --> updateRefs("revert")
+                                2. Discard Local changes   --> updateRefs("discard")
+                                3. Cancel
+                            */
+                        }
+                    }
+                    // jshint noempty:true
+                    return result;
+                });
             }
 
             return Promise(result);
@@ -391,7 +450,7 @@ exports.ProjectDocument = Document.specialize({
      * While typically not an operation that should be offered
      * while there are unsaved changes, doing so should perform
      * the merge as expected without committing those unsaved changes
-     * or discarding them. If desried, accepting or discarding unsaved changes
+     * or discarding them. If desired, accepting or discarding unsaved changes
      * should be done prior to calling merge.
      *
      *
@@ -401,9 +460,42 @@ exports.ProjectDocument = Document.specialize({
      */
     merge: {
         value: function (message, squash) {
+            var bridge = this._environmentBridge,
+                self = this,
+                result;
+
+            // jshint noempty:false
+            if (bridge && bridge.mergeShadowBranch && typeof bridge.mergeShadowBranch === "function") {
+                this.isBusy = true;
+
+                result = this._updateProjectRefs()
+                    .then(function(result) {
+                        if (result.success === true) {
+                            return bridge.mergeShadowBranch(self.currentBranch.name, message, squash)
+                                .then(function(result) {
+                                    if (result.success !== true) {
+                                        // cannot merge, check resolution and try again...
+                                        // TODO: ask user what to do or just cancel the merge
+                                    }
+                                })
+                                .then(function() {
+                                    self._updateShadowDelta();
+                                });
+                        } else {
+                            // repo not up-to-date, need sync
+                            //TODO: ask user to update local branches first
+                        }
+                    })
+                    .finally(function (result) {
+                        self.isBusy = false;
+                        return result;
+                    });
+            }
+            // jshint noempty:true
+
+            return Promise(result);
         }
     }
-
 },
 // Constructor Properties
 {
