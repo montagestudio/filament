d35cfd118db588621296eeb251a43521eab7d21e
François Frisch <francoisfrisch@gmail.com>
2014-04-04 15:27:48 -0700

    Move property-editor

--- ui/configurator.reel/configurator.js
+++ ui/configurator.reel/configurator.js
@@ -1,5 +1,7 @@
 var Montage = require("montage/core/core").Montage,
-    Panel = require("ui/panel.reel").Panel;
+    Promise = require("montage/core/promise").Promise,
+    Panel = require("ui/panel.reel").Panel,
+    MimeTypes = require("core/mime-types");
 
 exports.Configurator = Montage.create(Panel, {
 
@@ -43,8 +45,157 @@ exports.Configurator = Montage.create(Panel, {
                 } else {
                     this.inspectorControllers = null;
                 }
+
+                this.templateObject = selectedObject;
             }
         }
+    },
+
+
+    enterDocument: {
+        value: function (firstTime) {
+            if (!firstTime) {
+                return;
+            }
+
+            // Allow dropping events anywhere on the card
+            this.element.addEventListener("dragover", this, false);
+            this.element.addEventListener("dragleave", this, false);
+            this.element.addEventListener("drop", this, false);
+
+            // hover component in the stage
+            this.element.addEventListener("mouseover", this, false);
+            this.element.addEventListener("mouseout", this, false);
+
+            // selection
+            this.element.addEventListener("click", this, false);
+
+//            // save toggle state
+//            this.toggle.addEventListener("action", this, false);
+        }
+    },
+
+    _willAcceptDrop: {
+        value: false
+    },
+
+    _templateObject: {
+        value: null
+    },
+    templateObject: {
+        get: function() {
+            return this._templateObject;
+        },
+        set: function(value) {
+            if (this._templateObject === value) {
+                return;
+            }
+            this._templateObject = value;
+
+            if (value) {
+                var self = this;
+
+                this.canDrawGate.setField("needsObjectDescription", false);
+
+                this._describeTemplateObject()
+                    .spread(function (templateObject, description) {
+                        // Only accept values if the templateObject hasn't changed
+                        // since we went off to describe it
+                        if (templateObject === self._templateObject) {
+                            var keys = Object.keys(description);
+                            keys.forEach(function (key) {
+                                self[key] = description[key];
+                            });
+                            self.canDrawGate.setField("needsObjectDescription", true);
+                        }
+                    })
+                    .done();
+            }
+
+        }
+    },
+
+    _describeTemplateObject: {
+        value: function () {
+            var templateObject = this.templateObject,
+                packageRequire = templateObject.editingDocument.packageRequire,
+                description = {};
+
+            // Determine if this object is provided by the project's own package
+            // TODO not restrict this to components within the ui directory
+            description.isInProjectPackage = /^ui\//.test(templateObject.moduleId);
+
+            return Promise.all([
+                    packageRequire.async(templateObject.moduleId).get(templateObject.exportName),
+                    packageRequire.async("montage/ui/component").get("Component")
+                ])
+                .spread(function (objectConstructor, componentConstructor) {
+                    description.isTemplateObjectComponent = objectConstructor.prototype instanceof componentConstructor;
+                })
+                .fail(function () {
+                    description.isTemplateObjectComponent = null;
+                })
+                .thenResolve([templateObject, description]);
+        }
+    },
+
+    isInProjectPackage: {
+        value: null
+    },
+
+    isTemplateObjectComponent: {
+        value: null
+    },
+
+    handleDragover: {
+        value: function (event) {
+            var availableTypes = event.dataTransfer.types;
+
+            if (!availableTypes) {
+                event.dataTransfer.dropEffect = "none";
+                this._willAcceptDrop = false;
+            } else if (availableTypes.has(MimeTypes.MONTAGE_EVENT_TARGET)) {
+
+                // allows us to drop
+                event.preventDefault();
+                event.stopPropagation();
+                event.dataTransfer.dropEffect = "copy";
+                this._willAcceptDrop = true;
+            }
+        }
+    },
+
+    handleDragleave: {
+        value: function () {
+            this._willAcceptDrop = false;
+        }
+    },
+
+    handleDrop: {
+        value: function (event) {
+            var availableTypes = event.dataTransfer.types,
+                listenerModel;
+
+            // Always accept Events
+            if (availableTypes.has(MimeTypes.MONTAGE_EVENT_TARGET)) {
+
+                event.stopPropagation();
+                var eventTargetData = JSON.parse(event.dataTransfer.getData(MimeTypes.MONTAGE_EVENT_TARGET));
+
+                listenerModel = Object.create(null);
+                listenerModel.targetObject = this.templateObject.editingDocument.editingProxyMap[eventTargetData.targetLabel];
+                listenerModel.type = eventTargetData.eventType;
+                listenerModel.listener = this.templateObject;
+
+                this.dispatchEventNamed("addListenerForObject", true, false, {
+                    listenerModel: listenerModel
+                });
+
+            }
+
+            this._willAcceptDrop = false;
+        }
     }
+
 
 });
---
+++ ui/configurator.reel/listener-explorer.reel/listener-explorer.js
@@ -0,0 +1,206 @@
+/**
+ * @module ./listener-explorer.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component,
+    ObjectLabelConverter = require("core/object-label-converter").ObjectLabelConverter,
+    MimeTypes = require("core/mime-types");
+
+/**
+ * @class ListenerExplorer
+ * @extends Component
+ */
+exports.ListenerExplorer = Component.specialize(/** @lends ListenerExplorer# */ {
+
+    constructor: {
+        value: function ListenerExplorer() {
+            this.super();
+        }
+    },
+
+    _willAcceptDrop: {
+        value: false
+    },
+
+    acceptsDrop: {
+        value: function(event) {
+            return this.acceptsListenerDrop(event) || this.acceptsListenerLabelDrop(event);
+        }
+    },
+
+    acceptsListenerLabelDrop: {
+        value: function(event) {
+            var availableTypes = event.dataTransfer.types,
+                target = event.target,
+                element = this.element;
+
+            return availableTypes && availableTypes.has(MimeTypes.SERIALIZATION_OBJECT_LABEL) && (target === element || element.contains(target));
+        }
+    },
+
+    _getMontageUUID: {
+        value: function (types) {
+            if (!types) {
+                return false;
+            }
+            var uuid = false;
+            types.forEach(function (type, i) {
+                if (type.startsWith("x-montage-uuid")) {
+                    uuid = type.split("/")[1].toUpperCase();
+                }
+            });
+            return uuid;
+        }
+    },
+
+    acceptsListenerDrop: {
+        value: function(event) {
+            var availableTypes = event.dataTransfer.types,
+                target = event.target,
+                element = this.element,
+                uuid = this._getMontageUUID(availableTypes);
+            if (!availableTypes || !availableTypes.has(MimeTypes.MONTAGE_LISTENER)) {
+                return false;
+            }
+
+            if (uuid && (uuid  === this.templateObject.uuid)) {
+                return false;
+            }
+            return target === element || element.contains(target);
+        }
+    },
+
+    acceptsListenerCopy: {
+        value: function(evt) {
+            var addButton = this.element.querySelector("[data-montage-id=addListenerButton]");
+            return  addButton && evt.target === addButton;
+        }
+    },
+
+    enterDocument: {
+        value: function (firstTime) {
+            if (!firstTime) {
+                return;
+            }
+
+            // Allow event button to be dragged as a reference to this as an eventTarget
+            var eventButton = this.templateObjects.addListenerButton.element;
+            eventButton.addEventListener("dragstart", this, false);
+
+            // Allow dropping object references onto this component
+            var element = this.element;
+            element.addEventListener("dragover", this, false);
+            element.addEventListener("dragleave", this, false);
+            element.addEventListener("drop", this, false);
+        }
+    },
+
+    handleDragover: {
+        value: function (event) {
+            if (!this.acceptsDrop(event)) {
+                event.dataTransfer.dropEffect = "none";
+                this._willAcceptDrop = false;
+                return;
+            }
+
+            event.stop();
+            this._willAcceptDrop = true;
+
+            if (this.acceptsListenerDrop(event)) {
+                if (this.acceptsListenerCopy(event)) {
+                    event.dataTransfer.dropEffect = "copy";
+                } else {
+                    event.dataTransfer.dropEffect = "copy";
+                }
+            } else {
+                event.dataTransfer.dropEffect = "copy";
+            }
+        }
+    },
+
+    handleDragleave: {
+        value: function () {
+            this._willAcceptDrop = false;
+        }
+    },
+
+    handleDrop: {
+        value: function (event) {
+            var listenerModel,
+                self = this;
+
+            if (!this.acceptsDrop(event)) {
+                return;
+            }
+
+            if (this.acceptsListenerLabelDrop(event)) {
+                var listenerLabel = event.dataTransfer.getData(MimeTypes.SERIALIZATION_OBJECT_LABEL);
+                listenerModel = Object.create(null);
+                listenerModel.targetObject = this.templateObject;
+                listenerModel.listener = this.templateObject.editingDocument.editingProxyMap[listenerLabel];
+
+                this.dispatchEventNamed("addListenerForObject", true, false, {
+                    listenerModel: listenerModel
+                });
+            } else if (this.acceptsListenerDrop(event)) {
+                var data = event.dataTransfer.getData(MimeTypes.MONTAGE_LISTENER),
+                    targetObject = this.templateObject,
+                    // TODO: security issues?
+                    transferObject = JSON.parse(data),
+                    editingDocument = this.ownerComponent.ownerComponent.editingDocument,
+                    objectLabelConverter,
+                    listener;
+                if (!transferObject.listenerLabel) {
+                    return;
+                }
+                objectLabelConverter = new ObjectLabelConverter();
+                objectLabelConverter.editingDocument = editingDocument;
+                listener = objectLabelConverter.revert(transferObject.listenerLabel);
+
+                var move = !self.acceptsListenerCopy(event);
+                if (move) {
+                    editingDocument.undoManager.openBatch("Move Listener");
+                }
+
+                editingDocument.addOwnedObjectEventListener(targetObject, transferObject.type, listener, transferObject.useCapture, transferObject.methodName)
+                    .then(function() {
+                        // If this is not a copy, remove the other listener from whence this came
+                        if (move && transferObject.movedListenerIndex !== undefined && transferObject.movedListenerIndex > -1) {
+                            var fromTargetObject = objectLabelConverter.revert(transferObject.targeObjectLabel);
+                            if (fromTargetObject && fromTargetObject.listeners && fromTargetObject.listeners.length > transferObject.movedListenerIndex) {
+                                var listener = fromTargetObject.listeners[transferObject.movedListenerIndex];
+                                editingDocument.removeOwnedObjectEventListener(fromTargetObject, listener);
+                            }
+                        }
+                        if (move) {
+                            editingDocument.undoManager.closeBatch();
+                        }
+                    });
+            }
+
+            this._willAcceptDrop = false;
+        }
+    },
+
+    handleDragstart: {
+        value: function (evt) {
+            var target = evt.target,
+                listenerButtonElement = this.templateObjects.addListenerButton.element,
+                transfer = event.dataTransfer;
+
+            if (target === listenerButtonElement) {
+                transfer.effectAllowed = "copyMove";
+
+                var eventType = "action"; //TODO allow this to be inferred from somewhere
+                var transferObject = {
+                    targetLabel: this.templateObject.label,
+                    eventType: eventType
+                };
+
+                transfer.setData(MimeTypes.MONTAGE_EVENT_TARGET, JSON.stringify(transferObject));
+                transfer.setData("text/plain", eventType);
+            }
+        }
+    }
+
+});
