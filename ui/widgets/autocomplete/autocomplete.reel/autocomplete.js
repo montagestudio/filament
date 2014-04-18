f54b50904ee7b02fb738b38f9e1a1d9c414d5fda
Nikita Vasilyev <me@elv1s.ru>
2014-04-18 09:31:28 -0700

    Pressing enter on any field of the binding jig should create the binding.

--- ui/widgets/autocomplete/autocomplete.reel/autocomplete.js
+++ ui/widgets/autocomplete/autocomplete.reel/autocomplete.js
@@ -495,6 +495,12 @@ exports.Autocomplete = TextInput.specialize(/** @lends module:"matte/ui/autocomp
             var code = evt.keyCode,
                 popup = this._getPopup();
             switch(code) {
+            case KEY_ENTER:
+                if (popup.displayed) {
+                    evt.stop();
+                }
+                break;
+
             case KEY_DOWN:
                 evt.preventDefault();
                 break;
