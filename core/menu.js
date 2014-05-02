4d1cdeccb9e658f5d1309dab7e5128fc7f919aea
Mike Czepiel <mike@declarativ.com>
2014-05-02 14:49:20 -0700

    Changing goto to go
    Upholding our virtuous grammatical pursuits.

--- core/menu.js
+++ core/menu.js
@@ -176,9 +176,9 @@ Montage.defineProperty(exports, "defaultMenu", {
                 _defaultMenu.insertItem(viewMenu);
 
                 // Goto
-                gotoMenu = makeMenuItem("Goto", "", true, "", [
-                    makeMenuItem("Anything…", "goto", true, "command+e"),
-                    makeMenuItem("Anything Again…", "gotoAgain", true, "command+shift+e")
+                gotoMenu = makeMenuItem("Go", "", true, "", [
+                    makeMenuItem("Go to Anything…", "goto", true, "command+e"),
+                    makeMenuItem("Go to Anything Again…", "gotoAgain", true, "command+shift+e")
                 ]);
                 _defaultMenu.insertItem(gotoMenu);
 
