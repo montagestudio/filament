4c204dfd03cb728e55c9dbe3bcb22a60990ada91
François Frisch <francoisfrisch@gmail.com>
2014-05-08 16:55:29 -0700

    Fix template object selection bug

--- ui/component-editor.reel/component-editor.js
+++ ui/component-editor.reel/component-editor.js
@@ -257,12 +257,14 @@ exports.ComponentEditor = Editor.specialize({
     handleSelectComponent: {
         value: function (evt) {
             console.log("handleSelectComponent");
+            this.currentDocument.clearSelectedObjects();
             this.currentDocument.selectObject(evt.detail.templateObject);
         }
     },
 
     handleSelectTemplateObject: {
         value: function (evt) {
+            this.currentDocument.clearSelectedObjects();
             this.currentDocument.selectObject(evt.detail.templateObject);
         }
     },
