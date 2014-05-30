05968c9350073f1d80abd6425e7e144f1db8a301
Jean-Romain Prévost <jr@ailleurs.me>
2014-05-29 17:13:41 -0700

    Change mime type name

--- core/mime-types.js
+++ core/mime-types.js
@@ -47,7 +47,7 @@ exports.JSON_NODE = "x-filament/x-montage-json-node";
 
 // Represents a document targetLabel
 // Value is the document filename
-exports.DOCUMENTTAB = "x-filament/x-document-tab";
+exports.DOCUMENT_TAB = "x-filament/x-document-tab";
 
 // Represents a url
 exports.URL = "text/x-url";
--- ui/document-tab.reel/document-tab.js
+++ ui/document-tab.reel/document-tab.js
@@ -100,7 +100,7 @@ exports.DocumentTab = Component.specialize({
     handleDragstart:{
         value: function (evt) {
             var data = JSON.stringify({index: this.iteration.index});
-            evt.dataTransfer.setData(MimeTypes.DOCUMENTTAB, data);
+            evt.dataTransfer.setData(MimeTypes.DOCUMENT_TAB, data);
             evt.dataTransfer.setData(MimeTypes.URL, this.document.url);
             evt.dataTransfer.setData(MimeTypes.TEXT_PLAIN, this.document.url);
             // TODO: soft remove tab while dragging
