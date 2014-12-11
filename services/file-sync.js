4bb4eb59e834a0deae028ab8a23c150012918ff4
Pierre Chaussalet <pchaussalet@gmail.com>
2014-12-12 00:18:19 +0100

    Removes useless constructor in FileSyncService

--- services/file-sync.js
+++ services/file-sync.js
@@ -1,12 +1,6 @@
-var Montage = require("montage/core/core").Montage,
-    Promise = require("montage/core/promise").Promise;
+var Montage = require("montage/core/core").Montage;
 
 exports.FileSyncService = Montage.specialize({
-    constructor: {
-        value: function() {
-        }
-    },
-
     isInSync: {
         value: function(_document) {
             return _document.isDirty || _document.codeMirrorDocument.getValue() === _document.content;
