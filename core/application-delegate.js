85a1b24848516b1db2aa2cdd6b9bc4fcb0630a6c
Corentin Debost <codebost@gmail.com>
2018-03-25 22:56:34 -0700

    Combine application-delegate and firefly-application-delegate

--- core/application-delegate.js
+++ core/application-delegate.js
@@ -1,12 +1,16 @@
 var Montage = require("montage/core/core").Montage,
     Promise = require("montage/core/promise").Promise,
+    track = require("track"),
     ExtensionController = require("core/extension-controller").ExtensionController,
     ViewController = require("core/view-controller").ViewController,
     PreviewController = require("core/preview-controller").PreviewController,
     ProjectController = require("core/project-controller").ProjectController,
     ReelDocument = require("core/reel-document").ReelDocument,
     Document = require("palette/core/document").Document,
-    FilamentService = require("core/filament-service").FilamentService;
+    FilamentService = require("core/filament-service").FilamentService,
+    repositoriesController = require("project-list/core/repositories-controller").repositoriesController;
+
+var LICENSES = require("./licenses.html").content;
 
 exports.ApplicationDelegate = Montage.specialize({
 
@@ -62,12 +66,54 @@ exports.ApplicationDelegate = Montage.specialize({
         value: null
     },
 
+    _deferredRepositoryInitialization: {
+        value: null
+    },
+
+    progressPanel: {
+        value: null
+    },
+
+    promptPanel: {
+        value: null
+    },
+
+    initializeRepositoryPanel: {
+        value: null
+    },
+
+    unknownRepositoryPanel: {
+        value: null
+    },
+
+    mergePanel: {
+        value: null
+    },
+
+    mergeConflictPanel: {
+        value: null
+    },
+
+    showModal: {
+        value: false
+    },
+
+    isProjectLoaded: {
+        value: false
+    },
+
+    currentPanelKey: {
+        value: null
+    },
+
+
     constructor: {
         value: function () {
             // Make stack traces from promise errors easily available in the
             // console. Otherwise you need to manually inspect the error.stack
             // in the debugger.
             Promise.onerror = function (error) {
+                track.error(error);
                 if (error.stack) {
                     console.groupCollapsed("%c Uncaught promise rejection: " + (error.message || error), "color: #F00; font-weight: normal");
                     console.log(error.stack);
@@ -80,7 +126,7 @@ exports.ApplicationDelegate = Montage.specialize({
             this._deferredApplication = Promise.defer();
             this._deferredMainComponent = Promise.defer();
 
-            this.viewController = new ViewController;
+            this.viewController = new ViewController();
 
             this.previewController = (new PreviewController()).init(this);
 
@@ -174,18 +220,25 @@ exports.ApplicationDelegate = Montage.specialize({
     },
 
     updateStatusMessage: {
-        value: function() {
+        value: function(message) {
+            this.environmentBridge.progressPanel.message = message;
         }
     },
 
     /**
-     * Template method available for subclasses to implement their own logic
-     * as soon as an environment bridge is available
-     *
      * @return {Promise} A promise to continue
      */
     didLoadEnvironmentBridge: {
         value: function () {
+            //TODO the bridge and the appDelegate are fighting over responsibilities…
+            var bridge = this.environmentBridge;
+            bridge.progressPanel = this.progressPanel;
+            bridge.promptPanel = this.promptPanel;
+            bridge.confirmPanel = this.confirmPanel;
+            bridge.applicationDelegate = this;
+            bridge.userController.getUser().then(function (user) {
+                track.setUsername(user.login);
+            });
             return Promise.resolve();
         }
     },
@@ -198,24 +251,146 @@ exports.ApplicationDelegate = Montage.specialize({
      */
     willLoadProject: {
         value: function () {
-            return Promise.resolve();
+            var self = this,
+                bridge = this.environmentBridge;
+
+            self.showModal = true;
+            self.currentPanelKey = "progress";
+            self.progressPanel.message = "Verifying project…";
+
+            return bridge.repositoryExists()
+                .then(function (exists) {
+                    self.progressPanel.message = "Verifying repository…";
+
+                    return Promise.all([
+                        Promise.resolve(exists),
+                        (exists ? bridge.isProjectEmpty().then(function (empty) { return !empty; })
+                            : Promise.resolve(false)),
+                        bridge.workspaceExists()
+                    ]);
+                })
+                .spread(function (repositoryExists, repositoryPopulated, workspaceStatus) {
+                    var readyWorkspacePromise;
+
+                    if (!repositoryExists) {
+
+                        if (workspaceStatus === "initializing") {
+                            throw new Error("Initializing a project for a repository that no longer exists");
+                        } else if (workspaceStatus) {
+                            // Workspace exists, repository has gone missing
+                            // TODO offer to discard workspace or create repo from workspace
+                            throw new Error("Repository no longer exists for project.");
+                        } else {
+                            // No repo and no workspace
+                            self.currentPanelKey = "unknown";
+                            readyWorkspacePromise = self.unknownRepositoryPanel.getResponse()
+                                .then(function (response) {
+                                    if (response) {
+                                        return self._createRepository(response.name, response.description);
+                                    } else {
+                                        window.location = "/";
+                                    }
+                                });
+                        }
+                    } else {
+                        // The repository exists…
+                        if (repositoryPopulated) {
+                            if (workspaceStatus === "initializing" || !workspaceStatus) {
+                                // Project exists and is currently building the container workspace
+                                // or
+                                // Project is populated with content, no workspace exists
+                                // TODO check if we should bother creating a workspace for this repo
+                                // Hopefully it's not about to run minit to try and reinitialize the project
+                                readyWorkspacePromise = self._initializeProject();
+                            }
+                        } else {
+                            // The repository exists but is empty…
+                            if (workspaceStatus === "initializing") {
+                                // Busy initializing
+                                readyWorkspacePromise = self._initializeProject();
+                            } else if (workspaceStatus) {
+                                // Must have just wrapped up workspace will be pushed soon
+                                readyWorkspacePromise = self._initializeProject();
+                            } else {
+                                // Repository exists, is empty, and we have no workspace
+                                self.currentPanelKey = "initialize";
+                                readyWorkspacePromise = self.initializeRepositoryPanel.getResponse()
+                                    .then(function (response) {
+                                        if (response) {
+                                            return self._initializeProject();
+                                        } else {
+                                            window.location = "/";
+                                        }
+                                    });
+                            }
+                        }
+                    }
+
+                    return readyWorkspacePromise;
+                })
+                .catch(function(err) {
+                    var message = err.message || "Internal Server Error";
+
+                    self.currentPanelKey = "confirm";
+                    track.error(new Error("Error setting up the project: " + message));
+
+                    return self.confirmPanel.getResponse("Error setting up the project: " + message, true, "Retry", "Close")
+                        .then(function (response) {
+                            self.showModal = false;
+                            if (response === true) {
+                                return self.willLoadProject();
+                            } else {
+                                window.location = "/";
+                                return Promise.resolve(false);
+                            }
+                        });
+                });
         }
     },
 
     loadProject: {
         value: function(projectUrl) {
-            return this.projectController.loadProject(projectUrl);
+            var self = this;
+            return new Promise(function(resolve) {
+                var loadProject = function() {
+                    self.currentPanelKey = "progress";
+                    self.progressPanel.message = "Loading project…";
+                    self.showModal = true;
+                    track.message("loading project");
+                    self.projectController.loadProject(projectUrl)
+                        .then(resolve, loadProjectFail);
+                };
+
+                var loadProjectFail = function(reason) {
+                    var message = reason.message || "Internal Server Error";
+                    console.error(reason);
+                    self.currentPanelKey = "confirm";
+                    track.error("Couldn't load project: " + message);
+
+                    self.confirmPanel.getResponse("Error loading the project", true, "Retry", "Close")
+                        .then(function (response) {
+                            self.showModal = false;
+                            if (response === true) {
+                                loadProject();
+                            } else {
+                                window.location = "/";
+                            }
+                        });
+                };
+
+                loadProject();
+            });
         }
     },
 
     /**
-     * Template method available for subclasses to implement their own logic
-     * after a project has been loaded as directed by the environment
-     *
      * @return {Promise} A promise to continue
      */
     didLoadProject: {
         value: function () {
+            this.showModal = false;
+            this.isProjectLoaded = true;
+            this.currentPanelKey = null;
             return Promise.resolve();
         }
     },
@@ -242,6 +417,11 @@ exports.ApplicationDelegate = Montage.specialize({
                 self.willClose();
             }, true);
 
+            // TODO this is a temporary workaround to redirect keyEquivalents to the
+            // toolbar as a last resort if they make it up here
+            app.addEventListener("keyPress", this);
+            app.addEventListener("menuAction", this, false);
+
             this._deferredApplication.resolve(app);
         }
     },
@@ -250,6 +430,91 @@ exports.ApplicationDelegate = Montage.specialize({
         value: function () {
             //TODO only if we're registered
             this.previewController.unregisterPreview().done();
+        }
+    },
+
+    _createRepository: {
+        value: function (name, description) {
+            var self = this,
+                bridge = this.environmentBridge;
+
+            this.currentPanelKey = "progress";
+            this.progressPanel.message = "Creating repository…";
+
+            return repositoriesController.createRepository(name, description)
+                .then(function () {
+
+                    // Delay reporting initialization to distribute progress more evenly
+                    Promise.delay(7000)
+                        .then(function (result) {
+                            self.currentPanelKey = "progress";
+                            self.progressPanel.message = "Initializing project and installing dependencies…";
+                        })
+                        .done();
+
+                    return bridge.initializeProject();
+                });
+        }
+    },
+
+    _initializeProject: {
+        value: function () {
+            var bridge = this.environmentBridge,
+                self = this,
+                initializationPromise;
+
+            if (bridge) {
+
+                self.currentPanelKey = "progress";
+                self.progressPanel.message = "Initializing project and installing dependencies…";
+
+                initializationPromise = bridge.initializeProject();
+            }
+
+            return Promise.resolve(initializationPromise);
+        }
+    },
+
+    handleMenuAction: {
+        value: function (evt) {
+            switch (evt.detail.identifier) {
+            case "documentation":
+                window.open("http://docs.montagestudio.com/montage-studio/");
+                break;
+            case "forum":
+                window.open("http://forum.montagestudio.com/");
+                break;
+            case "report":
+                window.location = "mailto:feedback@montagestudio.com";
+                break;
+            case "api":
+                window.open("http://docs.montagestudio.com/api/AbstractButton.html");
+                break;
+            case "framework":
+                window.open("http://docs.montagestudio.com/montagejs/");
+                break;
+            case "licenses":
+                var self = this;
+                this.showModal = true;
+                this.currentPanelKey = "info";
+                this.infoPanel.getResponse(LICENSES, "Close")
+                .then(function() {
+                    self.showModal = false;
+                    self.currentPanelKey = null;
+                }).done();
+                break;
+            }
+        }
+    },
+
+    handleKeyPress: {
+        value: function (evt) {
+            if ("menuAction" === evt.identifier) {
+                var component = evt.keyComposer.component;
+                if (typeof component.handleKeyPress === "function") {
+                    component.handleKeyPress(evt);
+                }
+            }
         }
     }
 });
