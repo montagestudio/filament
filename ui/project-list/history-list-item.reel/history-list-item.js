d677a2f11c927863a8deec4633c95194b38d3892
Corentin Debost <codebost@gmail.com>
2018-03-26 23:22:01 -0700

    Implement navigate to project

--- ui/project-list/history-list-item.reel/history-list-item.js
+++ ui/project-list/history-list-item.reel/history-list-item.js
@@ -4,5 +4,12 @@ exports.HistoryListItem = Component.specialize({
 
     repository: {
         value: null
+    },
+
+    handleDocumentNameAction: {
+        value: function () {
+            var path = ["", this.repository.owner.login, this.repository.name].join("/");
+            this.application.delegate.changeLocation(path);
+        }
     }
 });
