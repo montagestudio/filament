ffecd8bd255bfdc72fb9bb67c8e84f9d691cecee
Corentin Debost <codebost@gmail.com>
2018-04-12 06:21:54 -0700

    Add github authorization panel

---
+++ ui/github-authorization-panel.reel/github-authorization-panel.js
@@ -0,0 +1,217 @@
+var AuthorizationPanel = require("montage/ui/authorization-panel.reel").AuthorizationPanel,
+    Promise = require('montage/core/promise').Promise;
+
+/**
+ * @class GithubAuthorizationPanel
+ * @extends AuthorizationPanel
+ */
+exports.GithubAuthorizationPanel = AuthorizationPanel.specialize({
+
+    isAuthenticated: {
+        value: false
+    },
+
+    needsTutorial: {
+        value: true
+    },
+
+    enterDocument: {
+        value: function (firstTime) {
+            var self = this;
+            if (firstTime) {
+                var needsTutorial = JSON.parse(localStorage.getItem("needsTutorial"));
+
+                if (null === needsTutorial) {
+                    localStorage.setItem("needsTutorial", true);
+                    needsTutorial = true;
+                }
+
+                this.needsTutorial = needsTutorial;
+            }
+            this._getCredentials().then(function (credentials) {
+                 return self.service.authorize(credentials);
+            }).then(function (authorization) {
+                self.authorizationManagerPanel.approveAuthorization(authorization, self);
+            });
+        }
+    },
+
+    handleLoginButtonAction: {
+        value: function(event) {
+            var self = this;
+            self._getCredentials().catch(function () {
+                return self._openPopup("/auth/github").then(function (popup) {
+                    return self._pollForCredentials(popup).then(function (credentials) {
+                        return self._setCredentials(credentials);
+                    }).finally(function () {
+                        if (typeof popup.close === 'function') {
+                            popup.close();
+                        }
+                    });
+                });
+            }).then(function (credentials) {
+                 return self.service.authorize(credentials);
+            }).then(function (authorization) {
+                self.authorizationManagerPanel.approveAuthorization(authorization, self);
+            }).catch(function (error) {
+                console.error(error);
+                self.authorizationManagerPanel.cancelAuthorization();
+            });
+        }
+    },
+
+    logout: {
+        value: function() {
+            this._setCredentials("");
+            this.authorizationManagerPanel.cancelAuthorization();
+        }
+    },
+
+    _pollForCredentials: {
+        value: function (popup) {
+            var self = this,
+                pollingInterval = 100,
+                polling;
+
+            return new Promise(function (resolve, reject) {
+                if (!popup) {
+                    reject('Authorization Failed (POPUP_BLOCKED)');
+                } else {
+                    polling = setTimeout(function tryPollPopup() {
+                        try {
+                            var documentOrigin = window.location.host,
+                                popupWindowLocation = popup.location,
+                                popupWindowOrigin = popupWindowLocation.host;
+
+                            if (popupWindowOrigin === documentOrigin) {
+                                if (popup.document.readyState === "complete") {
+                                    resolve(new URL(popupWindowLocation));
+                                }
+                            }
+                        } catch (err) {
+                            console.error(err);
+                            // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
+                        } finally {
+                            // we're here when the child window has been navigated away or closed
+                            if (!popup || popup.closed || popup.closed === undefined) {
+                                reject('Authorization Failed (LOAD_CANCEL)');
+                            } else if (popup) {
+                                // we're here when the child window returned to our domain
+                                setTimeout(tryPollPopup, pollingInterval);
+                            }
+                        }
+                    }, pollingInterval);
+                }
+            }).then(function (popupWindowLocation) {
+                return new Promise(function (resolve, reject) {
+                    var token = self._getHashParam(popupWindowLocation, 'result'),
+                        error = self._getHashParam(popupWindowLocation, 'error');
+
+                    if (error) {
+                        return reject(error);
+                    } else if (!token) {
+                        return reject("No token");
+                    } else {
+                       return resolve(token);
+                    }
+                });
+            });
+        }
+    },
+
+    _openPopup: {
+        value: function (url, options) {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+
+                var popupWindow,
+                    popupOptions = self._stringifyOptions(self._prepareOptions(options)),
+                    popupName = 'authorization-panel';
+
+                 popupWindow = window.open(url, popupName, popupOptions);
+
+                 if (popupWindow) {
+                    resolve(popupWindow);
+                 } else {
+                    reject(new Error('Unable to open popup'));
+                 }
+            });
+        }
+    },
+
+    _prepareOptions: {
+        value: function (options) {
+            options = options || {};
+
+            var width = options.width || 500,
+                height = options.height || 500,
+                preparedOptions = {
+                    width: width,
+                    height: height,
+                    left: parseInt(window.screenX + ((window.outerWidth - width) / 2)),
+                    top: parseInt(window.screenY + ((window.outerHeight - height) / 2.5)),
+                    toolbar: "no",
+                    location: "no",
+                    directories: "no",
+                    status: "no",
+                    menubar: "no",
+                    scrollbars: "yes",
+                    resizable: "no",
+                    copyhistory: "no"
+                };
+
+            Object.keys(options).forEach(function(key) {
+                preparedOptions[key] = options[key];
+            });
+
+            return preparedOptions;
+        }
+    },
+
+    _stringifyOptions: {
+        value: function (options) {
+            var parts = [];
+            Object.keys(options).forEach(function(key) {
+                parts.push(key + '=' + options[key]);
+            });
+
+            return parts.join(',');
+        }
+    },
+
+    _getHashParam: {
+        value: function (url, name) {
+            name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+            var regex = new RegExp('[#&?]' + name + '=([^;]*)'),
+                results = regex.exec(url.search || url.hash);
+
+            return results === null ? '' : JSON.parse(decodeURIComponent(results[1].replace(/\+/g, ' ')));
+        }
+    },
+
+    _getCredentials: {
+        value: function () {
+            return new Promise(function (resolve, reject) {
+                var credentials = localStorage.getItem('github-credentials', credentials);
+                if (credentials) {
+                    resolve(JSON.parse(credentials));
+                } else {
+                    reject(new Error('No credentials'));
+                }
+            });
+        }
+    },
+
+    _setCredentials: {
+        value: function (credentials) {
+            return new Promise(function (resolve, reject) {
+                if (!credentials) {
+                    reject(new Error('No credentials'));
+                } else {
+                    localStorage.setItem('github-credentials', JSON.stringify(credentials));
+                    resolve(credentials);
+                }
+            });
+        }
+    }
+});
